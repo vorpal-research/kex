bbbdc79331a2c0dae532e259fd1c0a060ecd8904
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-02-07 13:36:05 +0300

    configs

--- src/main/kotlin/org/jetbrains/research/kex/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kex/Main.kt
@@ -1,5 +1,56 @@
 package org.jetbrains.research.kex
 
+import org.jetbrains.research.kex.config.CmdConfig
+import org.jetbrains.research.kex.config.GlobalConfig
+import org.jetbrains.research.kex.config.PropertyConfig
+import org.jetbrains.research.kex.driver.SootDriver
+import soot.jimple.JimpleBody
+import soot.shimple.Shimple
+import soot.shimple.ShimpleBody
+
 fun main(args: Array<String>) {
-    println("Kex")
+    val cmd = CmdConfig(args)
+    val propertiesFile = cmd.getStringValue("properties", "system.properties")
+    GlobalConfig.initialize(
+            listOf(
+                    cmd,
+                    PropertyConfig(propertiesFile)
+            )
+    )
+
+    val driver = SootDriver.instance
+    val jar = GlobalConfig.instance.getStringValue("jar", "")
+    val mainClass = GlobalConfig.instance.getStringValue("main", "")
+
+    if (jar.isEmpty() or mainClass.isEmpty()) {
+        cmd.printHelp()
+        System.exit(1)
+    }
+
+    println("Analyzing package $jar with main class $mainClass")
+    driver.setup(jar, mainClass)
+    var classCount = 0
+    var methodCount = 0
+    driver.getClasses()
+            .filter { !it.name.matches(Regex("java.*|kotlin.*|sun.*")) }
+            .forEach { sc ->
+                println(sc.name)
+                sc.methods.forEach {
+                    if (it.hasActiveBody()) {
+                        val shimple = when (it.activeBody) {
+                            is ShimpleBody -> it.activeBody as ShimpleBody
+                            is JimpleBody -> Shimple.v().newBody(it)
+                            else -> {
+                                println("No body for ${it.name}")
+                                Shimple.v().newBody(it)
+                            }
+                        }
+                        println(shimple)
+                        println()
+                        ++methodCount
+                    }
+                }
+                ++classCount
+            }
+    println("Total $classCount classes; $methodCount methods")
 }
---
+++ src/main/kotlin/org/jetbrains/research/kex/config/Config.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.research.kex.config
+
+interface Config {
+
+    fun getStringValue(param: String): String?
+
+    fun getBooleanValue(param: String): Boolean? = getStringValue(param)?.toBoolean()
+    fun getIntValue(param: String): Int? = getStringValue(param)?.toInt()
+    fun getDoubleValue(param: String): Double? = getStringValue(param)?.toDouble()
+
+    fun getBooleanValue(param: String, default: Boolean = false) = getBooleanValue(param) ?: default
+    fun getStringValue(param: String, default: String = "") = getStringValue(param) ?: default
+    fun getIntValue(param: String, default: Int = 0) = getStringValue(param)?.toInt() ?: default
+    fun getDoubleValue(param: String, default: Double = 0.0) = getStringValue(param)?.toDouble() ?: default
+
+    fun getMultipleStringValue(param: String, delimeter: String = ",")
+            = getStringValue(param)?.split(delimeter)?.map { it.replace("\\s".toRegex(), "") }?.toList() ?: listOf()
+
+    fun getMultipleIntValue(param: String, delimeter: String = ",")
+            = getMultipleStringValue(param, delimeter).map { it.toInt() }
+
+    fun getMultipleDoubleValue(param: String, delimeter: String = ",")
+            = getMultipleStringValue(param, delimeter).map { it.toDouble() }
+}
