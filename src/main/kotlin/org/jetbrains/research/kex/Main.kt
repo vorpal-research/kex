d0851d7080e415b319ff94fa8f9552a854fc19c5
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-03-27 12:57:28 +0300

    using kfg

--- src/main/kotlin/org/jetbrains/research/kex/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kex/Main.kt
@@ -3,18 +3,35 @@ package org.jetbrains.research.kex
 import org.jetbrains.research.kex.config.CmdConfig
 import org.jetbrains.research.kex.config.GlobalConfig
 import org.jetbrains.research.kex.config.FileConfig
+import org.jetbrains.research.kex.util.loggerFor
+import org.jetbrains.research.kex.util.debug
+import org.jetbrains.research.kfg.CM
+import org.jetbrains.research.kfg.Package
+import org.jetbrains.research.kfg.util.writeClass
 import uk.org.lidalia.sysoutslf4j.context.LogLevel
 import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J
+import java.util.jar.JarFile
 
 fun main(args: Array<String>) {
     SysOutOverSLF4J.sendSystemOutAndErrToSLF4J(LogLevel.DEBUG, LogLevel.ERROR)
+    val log = loggerFor("org.jetbrains.research.kex.Main")
 
     val config = GlobalConfig
     val cmd = CmdConfig(args)
     val properties = cmd.getStringValue("properties", "system.properties")
     config.initialize(listOf(cmd, FileConfig(properties)))
 
-    val jarName = config.getStringValue("jar", "")
-    val packageName = config.getStringValue("package", "")
-    assert(jarName.isEmpty() or packageName.isEmpty(), cmd::printHelp)
+    val jarName = config.getStringValue("jar")
+    val packageName = config.getStringValue("package", "*")
+    assert(jarName != null, cmd::printHelp)
+
+    val jar = JarFile(jarName)
+    val `package` = Package(packageName.replace('.', '/'))
+    CM.parseJar(jar, `package`)
+
+    log.debug("Running with jar ${jar.name} and package $`package`")
+    for (it in CM.classes.values) {
+        log.debug(it)
+        writeClass(it, "${it.name}.class")
+    }
 }
