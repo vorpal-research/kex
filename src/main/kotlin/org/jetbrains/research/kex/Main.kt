a4c57c45b8d73d334598548b798f01ebd29dc270
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-03-30 16:28:07 +0300

    runner

--- src/main/kotlin/org/jetbrains/research/kex/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kex/Main.kt
@@ -5,15 +5,16 @@ import org.jetbrains.research.kex.config.GlobalConfig
 import org.jetbrains.research.kex.config.FileConfig
 import org.jetbrains.research.kex.util.loggerFor
 import org.jetbrains.research.kex.asm.JumpInstrumenter
+import org.jetbrains.research.kex.runner.Runner
 import org.jetbrains.research.kfg.CM
 import org.jetbrains.research.kfg.Package
+import org.jetbrains.research.kfg.util.writeClass
 import org.jetbrains.research.kfg.util.writeJar
-import uk.org.lidalia.sysoutslf4j.context.LogLevel
-import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J
+import java.io.File
+import java.net.URLClassLoader
 import java.util.jar.JarFile
 
 fun main(args: Array<String>) {
-    SysOutOverSLF4J.sendSystemOutAndErrToSLF4J(LogLevel.DEBUG, LogLevel.ERROR)
     val log = loggerFor("org.jetbrains.research.kex.Main")
 
     val config = GlobalConfig
@@ -30,10 +31,17 @@ fun main(args: Array<String>) {
     CM.parseJar(jar, `package`)
 
     log.debug("Running with jar ${jar.name} and package $`package`")
-    for (it in CM.getConcreteClasses()) {
-        for ((_, method) in it.methods) {
-            if (!method.isAbstract()) JumpInstrumenter(method).visit()
+    val target = File("instrumented/")
+    writeJar(jar, `package`, target)
+
+    for (`class` in CM.getConcreteClasses()) {
+        for ((_, method) in `class`.methods) {
+            if (!method.isAbstract() && method.name != "<init>") {
+                JumpInstrumenter(method).visit()
+                writeClass(`class`, "${target.canonicalPath}/${`class`.getFullname()}.class")
+                val loader = URLClassLoader(arrayOf(target.toURI().toURL()))
+                Runner(method, loader).run()
+            }
         }
     }
-    writeJar(jar, `package`, "instrumented")
 }
