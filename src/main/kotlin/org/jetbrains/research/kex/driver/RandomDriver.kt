533f44484914ed023c52a59769e083272e22c774
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-04-04 16:47:57 +0300

    random method args generation

---
+++ src/main/kotlin/org/jetbrains/research/kex/driver/RandomDriver.kt
@@ -0,0 +1,64 @@
+package org.jetbrains.research.kex.driver
+
+import io.github.benas.randombeans.EnhancedRandomBuilder
+import io.github.benas.randombeans.api.EnhancedRandom
+import io.github.benas.randombeans.api.ObjectGenerationException
+import org.jetbrains.research.kex.UnexpectedException
+import org.jetbrains.research.kex.config.GlobalConfig
+import org.jetbrains.research.kex.util.Loggable
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import java.lang.reflect.TypeVariable
+
+internal val minCollectionSize = GlobalConfig.getIntValue("random.minCollectionSize", 0)
+internal val maxCollectionSize = GlobalConfig.getIntValue("random.maxCollectionSize", 1000)
+internal val minStringLength = GlobalConfig.getIntValue("random.minStringLength", 0)
+internal val maxStringLength = GlobalConfig.getIntValue("random.maxStringLength", 1000)
+internal val attempts = GlobalConfig.getIntValue("random.generationAttempts", 1)
+
+class RandomDriver : Loggable {
+    private val randomizer: EnhancedRandom = EnhancedRandomBuilder.aNewEnhancedRandomBuilder()
+            .scanClasspathForConcreteTypes(true)
+            .collectionSizeRange(minCollectionSize, maxCollectionSize)
+            .stringLengthRange(minStringLength, maxStringLength)
+            .build()
+
+    private fun <T> generateClass(`class`: Class<T>) = randomizer.nextObject(`class`)
+
+    private fun generateParameterized(type: ParameterizedType): Any {
+        val rawType = type.rawType
+        val `object` = generate(rawType)
+        if (rawType is Class<*>) {
+            val typeParams = rawType.typeParameters.zip(type.actualTypeArguments).toMap()
+            for (it in rawType.declaredFields) {
+                val genType = typeParams[it.genericType as? TypeVariable<*>] ?: it.genericType
+                it.isAccessible = true
+                val value = generate(genType)
+                it.set(`object`, value)
+            }
+        } else throw UnexpectedException("Unknown type $type")
+        return `object`
+    }
+
+    private fun generateTypeVariable(type: TypeVariable<*>): Any {
+        val bounds = type.bounds
+        assert(bounds.size == 1, { log.debug("Unexpected size of type variable bounds: ${bounds.map { it.typeName }}") })
+        return generate(bounds.first())
+    }
+
+    fun generate(type: Type): Any {
+        repeat(attempts, {
+            try {
+                return when (type) {
+                    is Class<*> -> generateClass(type)
+                    is ParameterizedType -> generateParameterized(type)
+                    is TypeVariable<*> -> generateTypeVariable(type)
+                    else -> throw UnexpectedException("Unknown type $type")
+                }
+            } catch (exc: ObjectGenerationException) {
+                log.debug("Failed when trying to generate object: ${exc.message}")
+            }
+        })
+        throw ObjectGenerationException("Unable to generate a random instance of type $type")
+    }
+}
--- src/main/kotlin/org/jetbrains/research/kex/runner/Runner.kt
+++ src/main/kotlin/org/jetbrains/research/kex/runner/Runner.kt
@@ -1,14 +1,15 @@
 package org.jetbrains.research.kex.runner
 
-import io.github.benas.randombeans.api.EnhancedRandom.random
+import org.jetbrains.research.kex.driver.RandomDriver
 import org.jetbrains.research.kex.util.Loggable
 import org.jetbrains.research.kex.util.debug
 import org.jetbrains.research.kfg.ir.Method
 import org.jetbrains.research.kfg.type.*
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+import java.lang.reflect.InvocationTargetException
 
-fun getClass(type: Type): Class<*> = when (type) {
+internal fun getClass(type: Type, loader: ClassLoader): Class<*> = when (type) {
     is BoolType -> Boolean::class.java
     is ByteType -> Byte::class.java
     is ShortType -> Short::class.java
@@ -17,35 +18,45 @@ fun getClass(type: Type): Class<*> = when (type) {
     is CharType -> Char::class.java
     is FloatType -> Float::class.java
     is DoubleType -> Double::class.java
-    else -> try { Class.forName(type.getCanonicalDesc()) } catch (e: ClassNotFoundException) { ClassLoader.getSystemClassLoader().loadClass(type.getCanonicalDesc().drop(1).dropLast(1)) }
+    is ArrayType -> Class.forName(type.getCanonicalDesc())
+    is ClassType -> try {
+        loader.loadClass(type.`class`.getFullname().replace('/', '.'))
+    } catch (e: ClassNotFoundException) {
+        ClassLoader.getSystemClassLoader().loadClass(type.`class`.getFullname().replace('/', '.'))
+    }
+    else -> TODO()
 }
 
 class Runner(val method: Method, val loader: ClassLoader) : Loggable {
     val `class` = method.`class`
     val out = ByteArrayOutputStream()
+    val random = RandomDriver()
 
     fun run() {
+        log.debug("Running $method")
+
         val javaClass = loader.loadClass(`class`.getFullname().replace('/', '.'))
-        val argumentTypes = method.desc.args.map { getClass(it) }.toTypedArray()
+        val argumentTypes = method.desc.args.map { getClass(it, loader) }.toTypedArray()
         val javaMethod = javaClass.getDeclaredMethod(method.name, *argumentTypes)
 
-        val instance = if (method.isStatic()) null else random(javaClass)
+        val instance = if (method.isStatic()) null else random.generate(javaClass)
         if (!javaMethod.isAccessible) javaMethod.isAccessible = true
-        val args = argumentTypes.map {
-            random(it)
-        }.toTypedArray()
+        val args = javaMethod.genericParameterTypes.map { random.generate(it) }.toTypedArray()
 
-        log.debug(method)
         log.debug("Instance: $instance")
         log.debug("Generated args: ${args.map { it.toString() }}")
 
         val oldOut = System.out
-        System.setOut(PrintStream(out))
-        javaMethod.invoke(instance, *args)
-        System.setOut(oldOut)
-
-        log.debug("Invokation output:")
-        log.debug(out)
+        try {
+            System.setOut(PrintStream(out))
+            javaMethod.invoke(instance, *args)
+            System.setOut(oldOut)
+            log.debug("Invokation output:")
+            log.debug(out)
+        } catch (e: InvocationTargetException) {
+            System.setOut(oldOut)
+            log.debug("Invocation exception ${e.targetException}")
+        }
         log.debug()
     }
 }
