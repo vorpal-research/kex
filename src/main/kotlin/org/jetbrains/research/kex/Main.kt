af829cb5bd31e1f3cfdc63b6dce3b8229e62cf12
Azat Abdullin <azat.aam@gmail.com>
2018-04-11 13:41:55 +0300

    coverage runner

--- src/main/kotlin/org/jetbrains/research/kex/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kex/Main.kt
@@ -6,6 +6,7 @@ import org.jetbrains.research.kex.config.FileConfig
 import org.jetbrains.research.kex.util.loggerFor
 import org.jetbrains.research.kex.asm.TraceInstrumenter
 import org.jetbrains.research.kex.runner.CoverageRunner
+import org.jetbrains.research.kex.runner.CoverageManager
 import org.jetbrains.research.kfg.CM
 import org.jetbrains.research.kfg.Package
 import org.jetbrains.research.kfg.util.writeClass
---
+++ src/main/kotlin/org/jetbrains/research/kex/runner/CoverageManager.kt
@@ -0,0 +1,122 @@
+package org.jetbrains.research.kex.runner
+
+import org.jetbrains.research.kex.util.loggerFor
+import org.jetbrains.research.kfg.ir.BasicBlock
+import org.jetbrains.research.kfg.ir.Method
+import java.util.*
+
+data class BlockInfo internal constructor(val bb: BasicBlock, val predecessor: BlockInfo?) {
+    var outputAction: BlockExitAction? = null
+        internal set
+
+    fun hasPredecessor() = predecessor != null
+    fun hasOutput() = outputAction != null
+}
+
+class MethodInfo internal constructor(val method: Method, val instance: ActionValue?,
+                                      val args: Array<ActionValue>, val blocks: Map<BasicBlock, List<BlockInfo>>,
+                                      val retval: ActionValue?, val throwval: ActionValue?) {
+    fun getBlockInfo(bb: BasicBlock) = blocks.getValue(bb)
+
+    companion object {
+        fun parse(actions: List<Action>): List<MethodInfo> {
+            val log = loggerFor(this::class.java)
+
+            class Info(var instance: ActionValue?,
+                       var args: Array<ActionValue>, var blocks: MutableMap<BasicBlock, MutableList<BlockInfo>>,
+                       var retval: ActionValue?, var throwval: ActionValue?)
+
+            val infos = Stack<Info>()
+            val methodStack = Stack<Method>()
+            val result = mutableListOf<MethodInfo>()
+            var previousBlock: BlockInfo? = null
+            for (action in actions) {
+                when (action) {
+                    is MethodEntry -> {
+                        methodStack.push(action.method)
+                        infos.push(Info(null, arrayOf(), mutableMapOf(), null, null))
+                    }
+                    is MethodInstance -> {
+                        assert(methodStack.peek() == action.method, { log.error("Incorrect action format: Instance action for wrong method") })
+                        val info = infos.peek()
+                        info.instance = action.instance.rhv
+                    }
+                    is MethodArgs -> {
+                        assert(methodStack.peek() == action.method, { log.error("Incorrect action format: Arg action for wrong method") })
+                        val info = infos.peek()
+                        info.args = action.args.map { it.rhv }.toTypedArray()
+                    }
+                    is MethodReturn -> {
+                        assert(methodStack.peek() == action.method, { log.error("Incorrect action format: Return action for wrong method") })
+                        val info = infos.peek()
+                        info.retval = action.`return`?.rhv
+
+                        result.add(MethodInfo(methodStack.pop(), info.instance, info.args, info.blocks, info.retval, info.throwval))
+                        infos.pop()
+                    }
+                    is MethodThrow -> {
+                        assert(methodStack.peek() == action.method, { log.error("Incorrect action format: Throw action for wrong method") })
+                        val info = infos.peek()
+                        info.throwval = action.throwable.rhv
+
+                        result.add(MethodInfo(methodStack.pop(), info.instance, info.args, info.blocks, info.retval, info.throwval))
+                        infos.pop()
+                    }
+                    is BlockEntry -> {
+                        val bb = action.bb
+                        assert(methodStack.peek() == bb.parent, { log.error("Incorrect action format: Block action for wrong method") })
+                        val info = infos.peek()
+
+                        val bInfo = BlockInfo(bb, previousBlock)
+                        previousBlock = bInfo
+                        info.blocks.getOrPut(bb, { mutableListOf() }).add(bInfo)
+                    }
+                    is BlockJump -> {
+                        val bb = action.bb
+                        assert(methodStack.peek() == bb.parent, { log.error("Incorrect action format: Block action for wrong method") })
+                        assert(previousBlock != null, { log.error("Incorrect action format: Block exit without entering") })
+                        previousBlock?.outputAction = action
+                    }
+                    is BlockBranch -> {
+                        val bb = action.bb
+                        assert(methodStack.peek() == bb.parent, { log.error("Incorrect action format: Block action for wrong method") })
+                        assert(previousBlock != null, { log.error("Incorrect action format: Block exit without entering") })
+                        previousBlock?.outputAction = action
+                    }
+                    is BlockSwitch -> {
+                        val bb = action.bb
+                        assert(methodStack.peek() == bb.parent, { log.error("Incorrect action format: Block action for wrong method") })
+                        assert(previousBlock != null, { log.error("Incorrect action format: Block exit without entering") })
+                        previousBlock?.outputAction = action
+                    }
+                    is BlockTableSwitch -> {
+                        val bb = action.bb
+                        assert(methodStack.peek() == bb.parent, { log.error("Incorrect action format: Block action for wrong method") })
+                        assert(previousBlock != null, { log.error("Incorrect action format: Block exit without entering") })
+                        previousBlock?.outputAction = action
+                    }
+                }
+            }
+            while (methodStack.isNotEmpty()) {
+                val info = infos.peek()
+                result.add(MethodInfo(methodStack.pop(), info.instance, info.args, info.blocks, info.retval, info.throwval))
+                infos.pop()
+            }
+            return result
+        }
+    }
+}
+
+object CoverageManager {
+    private val methods = mutableMapOf<Method, MutableList<MethodInfo>>()
+
+    fun getMethodInfos(method: Method): List<MethodInfo> = methods.getOrPut(method, { mutableListOf() })
+    fun getBlockInfos(bb: BasicBlock): List<BlockInfo> = getMethodInfos(bb.parent).map {
+        it.blocks[bb] ?: listOf()
+    }.flatten()
+
+    fun addInfo(method: Method, info: MethodInfo) = methods.getOrPut(method, { mutableListOf() }).add(info)
+
+    fun isCovered(bb: BasicBlock) = getBlockInfos(bb).fold(false, { acc, it -> acc or it.hasOutput() })
+    fun isCovered(method: Method) = method.basicBlocks.map { isCovered(it) }.fold(true, { acc, it -> acc and it })
+}
