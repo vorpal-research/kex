83a513400be38bc5bbd05d7285b02890d24462d3
Azat Abdullin <azat.aam@gmail.com>
2018-05-25 18:13:40 +0300

    smt real

--- src/main/kotlin/org/jetbrains/research/kex/smt/SMTEngine.kt
+++ src/main/kotlin/org/jetbrains/research/kex/smt/SMTEngine.kt
@@ -41,11 +41,17 @@ abstract class SMTEngine<in Context_t : Any, Expr_t : Any, Sort_t : Any, Functio
     abstract fun getSort(ctx: Context_t, expr: Expr_t): Sort_t
     abstract fun getBoolSort(ctx: Context_t): Sort_t
     abstract fun getBVSort(ctx: Context_t, size: Int): Sort_t
+    abstract fun getFPSort(ctx: Context_t, esize: Int, ssize: Int): Sort_t
+    abstract fun getFloatSort(ctx: Context_t): Sort_t
+    abstract fun getDoubleSort(ctx: Context_t): Sort_t
     abstract fun getArraySort(ctx: Context_t, domain: Sort_t, range: Sort_t): Sort_t
     abstract fun isBool(ctx: Context_t, expr: Expr_t): Boolean
     abstract fun isBV(ctx: Context_t, expr: Expr_t): Boolean
+    abstract fun isFP(ctx: Context_t, expr: Expr_t): Boolean
     abstract fun isArray(ctx: Context_t, expr: Expr_t): Boolean
     abstract fun bvBitsize(ctx: Context_t, sort: Sort_t): Int
+    abstract fun fpEBitsize(ctx: Context_t, sort: Sort_t): Int
+    abstract fun fpSBitsize(ctx: Context_t, sort: Sort_t): Int
 
     abstract fun hash(ctx: Context_t, expr: Expr_t): Int
     abstract fun name(ctx: Context_t, expr: Expr_t): String
@@ -61,6 +67,9 @@ abstract class SMTEngine<in Context_t : Any, Expr_t : Any, Sort_t : Any, Functio
     abstract fun makeNumericConst(ctx: Context_t, value: Int): Expr_t
     abstract fun makeNumericConst(ctx: Context_t, value: Long): Expr_t
     abstract fun makeNumericConst(ctx: Context_t, sort: Sort_t, value: Long): Expr_t
+    abstract fun makeFPConst(ctx: Context_t, value: Int): Expr_t
+    abstract fun makeFPConst(ctx: Context_t, value: Float): Expr_t
+    abstract fun makeFPConst(ctx: Context_t, value: Double): Expr_t
     abstract fun makeArrayConst(ctx: Context_t, sort: Sort_t, expr: Expr_t): Expr_t
     abstract fun makeFunction(ctx: Context_t, name: String, retSort: Sort_t, args: List<Sort_t>): Function_t
 
@@ -97,117 +106,56 @@ class SMTEngineProxy<in Context_t : Any, Expr_t : Any, Sort_t : Any, Function_t 
         return method.invoke(engine, *args) ?: unreachable { log.error("$methodName invocation returned null") }
     }
 
+    @Suppress("UNCHECKED_CAST")
+    private fun <ResT> proxy(ctx: Context_t, methodName: String, vararg args: Any): ResT =
+            proxyInvoke(ctx, methodName, args) as? ResT
+                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
+
     private fun getMethodName() = Throwable().stackTrace.drop(1).first().methodName
 
-    @Suppress("UNCHECKED_CAST")
-    override fun getSort(ctx: Context_t, expr: Expr_t): Sort_t = proxyInvoke(ctx, getMethodName(), ctx, expr) as? Sort_t
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun getSort(ctx: Context_t, expr: Expr_t): Sort_t = proxy(ctx, getMethodName(), ctx, expr)
+    override fun getBoolSort(ctx: Context_t): Sort_t = proxy(ctx, getMethodName(), ctx)
+    override fun getBVSort(ctx: Context_t, size: Int): Sort_t = proxy(ctx, getMethodName(), ctx, size)
+    override fun getFPSort(ctx: Context_t, esize: Int, ssize: Int): Sort_t = proxy(ctx, getMethodName(), ctx, esize, ssize)
+    override fun getFloatSort(ctx: Context_t): Sort_t = proxy(ctx, getMethodName())
+    override fun getDoubleSort(ctx: Context_t): Sort_t = proxy(ctx, getMethodName())
+    override fun getArraySort(ctx: Context_t, domain: Sort_t, range: Sort_t): Sort_t = proxy(ctx, getMethodName(), ctx, domain, range)
 
-    @Suppress("UNCHECKED_CAST")
-    override fun getBoolSort(ctx: Context_t): Sort_t = proxyInvoke(ctx, getMethodName(), ctx) as? Sort_t
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun isBool(ctx: Context_t, expr: Expr_t): Boolean = proxy(ctx, getMethodName(), ctx, expr)
+    override fun isBV(ctx: Context_t, expr: Expr_t): Boolean = proxy(ctx, getMethodName(), ctx, expr)
+    override fun isFP(ctx: Context_t, expr: Expr_t): Boolean = proxy(ctx, getMethodName(), ctx, expr)
+    override fun isArray(ctx: Context_t, expr: Expr_t): Boolean = proxy(ctx, getMethodName(), ctx, expr)
+    override fun bvBitsize(ctx: Context_t, sort: Sort_t): Int = proxy(ctx, getMethodName(), ctx, sort)
+    override fun fpEBitsize(ctx: Context_t, sort: Sort_t): Int = proxy(ctx, getMethodName(), ctx, sort)
+    override fun fpSBitsize(ctx: Context_t, sort: Sort_t): Int = proxy(ctx, getMethodName(), ctx, sort)
 
-    @Suppress("UNCHECKED_CAST")
-    override fun getBVSort(ctx: Context_t, size: Int): Sort_t = proxyInvoke(ctx, getMethodName(), ctx, size) as? Sort_t
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun hash(ctx: Context_t, expr: Expr_t): Int = proxy(ctx, getMethodName(), ctx, expr)
+    override fun name(ctx: Context_t, expr: Expr_t): String = proxy(ctx, getMethodName(), ctx, expr)
+    override fun toString(ctx: Context_t, expr: Expr_t): String = proxy(ctx, getMethodName(), ctx, expr)
 
-    @Suppress("UNCHECKED_CAST")
-    override fun getArraySort(ctx: Context_t, domain: Sort_t, range: Sort_t): Sort_t =
-            proxyInvoke(ctx, getMethodName(), ctx, domain, range) as? Sort_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun simplify(ctx: Context_t, expr: Expr_t): Expr_t = proxy(ctx, getMethodName(), ctx, expr)
 
-    override fun isBool(ctx: Context_t, expr: Expr_t): Boolean = proxyInvoke(ctx, getMethodName(), ctx, expr) as? Boolean
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun equality(ctx: Context_t, lhv: Expr_t, rhv: Expr_t): Boolean = proxy(ctx, getMethodName(), ctx, lhv, rhv)
 
-    override fun isBV(ctx: Context_t, expr: Expr_t): Boolean = proxyInvoke(ctx, getMethodName(), ctx, expr) as? Boolean
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun makeVar(ctx: Context_t, sort: Sort_t, name: String): Expr_t = proxy(ctx, getMethodName(), ctx, sort, name)
 
-    override fun isArray(ctx: Context_t, expr: Expr_t): Boolean = proxyInvoke(ctx, getMethodName(), ctx, expr) as? Boolean
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun makeBoolConst(ctx: Context_t, value: Boolean): Expr_t = proxy(ctx, getMethodName(), ctx, value)
 
-    override fun bvBitsize(ctx: Context_t, sort: Sort_t): Int = proxyInvoke(ctx, getMethodName(), ctx, sort) as? Int
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun makeNumericConst(ctx: Context_t, value: Short): Expr_t = proxy(ctx, getMethodName(), value)
 
-    override fun hash(ctx: Context_t, expr: Expr_t): Int = proxyInvoke(ctx, getMethodName(), ctx, expr) as? Int
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    override fun name(ctx: Context_t, expr: Expr_t): String = proxyInvoke(ctx, getMethodName(), ctx, expr) as? String
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    override fun toString(ctx: Context_t, expr: Expr_t): String = proxyInvoke(ctx, getMethodName(), ctx, expr) as? String
-            ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun simplify(ctx: Context_t, expr: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), ctx, expr) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    override fun equality(ctx: Context_t, lhv: Expr_t, rhv: Expr_t): Boolean =
-            proxyInvoke(ctx, getMethodName(), ctx, lhv, rhv) as? Boolean
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeVar(ctx: Context_t, sort: Sort_t, name: String): Expr_t =
-            proxyInvoke(ctx, getMethodName(), ctx, sort, name) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeBoolConst(ctx: Context_t, value: Boolean): Expr_t =
-            proxyInvoke(ctx, getMethodName(), ctx, value) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeNumericConst(ctx: Context_t, value: Short): Expr_t =
-            proxyInvoke(ctx, getMethodName(), value) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeNumericConst(ctx: Context_t, value: Int): Expr_t =
-            proxyInvoke(ctx, getMethodName(), value) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeNumericConst(ctx: Context_t, value: Long): Expr_t =
-            proxyInvoke(ctx, getMethodName(), value) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeNumericConst(ctx: Context_t, sort: Sort_t, value: Long): Expr_t =
-            proxyInvoke(ctx, getMethodName(), sort, value) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun makeArrayConst(ctx: Context_t, sort: Sort_t, expr: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), sort, expr) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
+    override fun makeNumericConst(ctx: Context_t, value: Int): Expr_t = proxy(ctx, getMethodName(), value)
+    override fun makeNumericConst(ctx: Context_t, value: Long): Expr_t = proxy(ctx, getMethodName(), value)
+    override fun makeNumericConst(ctx: Context_t, sort: Sort_t, value: Long): Expr_t = proxy(ctx, getMethodName(), sort, value)
+    override fun makeFPConst(ctx: Context_t, value: Int): Expr_t = proxy(ctx, getMethodName(), value)
+    override fun makeFPConst(ctx: Context_t, value: Float): Expr_t = proxy(ctx, getMethodName(), value)
+    override fun makeFPConst(ctx: Context_t, value: Double): Expr_t = proxy(ctx, getMethodName(), value)
+    override fun makeArrayConst(ctx: Context_t, sort: Sort_t, expr: Expr_t): Expr_t = proxy(ctx, getMethodName(), sort, expr)
     override fun makeFunction(ctx: Context_t, name: String, retSort: Sort_t, args: List<Sort_t>): Function_t =
-            proxyInvoke(ctx, getMethodName(), name, retSort, args) as? Function_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
+            proxy(ctx, getMethodName(), name, retSort, args)
 
-    @Suppress("UNCHECKED_CAST")
-    override fun apply(ctx: Context_t, f: Function_t, args: List<Expr_t>): Expr_t =
-            proxyInvoke(ctx, getMethodName(), f, args) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun negate(ctx: Context_t, expr: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), expr) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun binary(ctx: Context_t, opcode: Opcode, lhv: Expr_t, rhv: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), opcode, lhv, rhv) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun conjunction(ctx: Context_t, vararg exprs: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), exprs) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
-
-    @Suppress("UNCHECKED_CAST")
-    override fun sext(ctx: Context_t, n: Int, expr: Expr_t): Expr_t =
-            proxyInvoke(ctx, getMethodName(), n, expr) as? Expr_t
-                    ?: unreachable { log.error("SMT engine returned incorrect type object") }
+    override fun apply(ctx: Context_t, f: Function_t, args: List<Expr_t>): Expr_t = proxy(ctx, getMethodName(), f, args)
+    override fun negate(ctx: Context_t, expr: Expr_t): Expr_t = proxy(ctx, getMethodName(), expr)
+    override fun binary(ctx: Context_t, opcode: Opcode, lhv: Expr_t, rhv: Expr_t): Expr_t = proxy(ctx, getMethodName(), opcode, lhv, rhv)
+    override fun conjunction(ctx: Context_t, vararg exprs: Expr_t): Expr_t = proxy(ctx, getMethodName(), exprs)
+    override fun sext(ctx: Context_t, n: Int, expr: Expr_t): Expr_t = proxy(ctx, getMethodName(), n, expr)
 }
