aab1d3b851452904601fa1a3508618777b06d5cb
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-07-11 12:33:20 +0300

    runner option

--- core/src/main/kotlin/org/jetbrains/research/kex/Main.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/Main.kt
@@ -2,9 +2,12 @@ package org.jetbrains.research.kex
 
 import org.jetbrains.research.kex.asm.state.PredicateStateAnalysis
 import org.jetbrains.research.kex.asm.transform.LoopDeroller
+import org.jetbrains.research.kex.asm.transform.TraceInstrumenter
 import org.jetbrains.research.kex.config.CmdConfig
 import org.jetbrains.research.kex.config.GlobalConfig
 import org.jetbrains.research.kex.config.FileConfig
+import org.jetbrains.research.kex.runner.CoverageManager
+import org.jetbrains.research.kex.runner.CoverageRunner
 import org.jetbrains.research.kex.smt.z3.*
 import org.jetbrains.research.kex.state.transformer.*
 import org.jetbrains.research.kex.util.debug
@@ -14,8 +17,10 @@ import org.jetbrains.research.kfg.Package
 import org.jetbrains.research.kfg.analysis.IRVerifier
 import org.jetbrains.research.kfg.analysis.LoopAnalysis
 import org.jetbrains.research.kfg.analysis.LoopSimplifier
+import org.jetbrains.research.kfg.util.writeClass
 import org.jetbrains.research.kfg.util.writeClassesToTarget
 import java.io.File
+import java.net.URLClassLoader
 import java.util.jar.JarFile
 
 fun main(args: Array<String>) {
@@ -38,34 +43,37 @@ fun main(args: Array<String>) {
     val target = File("instrumented/")
     writeClassesToTarget(jar, target, `package`, true) // write all classes to target, so they will be seen by ClassLoader
 
-//    for (`class` in CM.getConcreteClasses()) {
-//        for ((_, method) in `class`.methods) {
-//            val classFileName = "${target.canonicalPath}/${`class`.getFullname()}.class"
-//            if (!method.isAbstract() && method.name != "<init>" && method.name != "<clinit>") {
-//                val instrumenter = TraceInstrumenter(method)
-//                instrumenter.visit()
-//                writeClass(`class`, classFileName)
-//                val loader = URLClassLoader(arrayOf(target.toURI().toURL()))
-//                CoverageRunner(method, loader).run()
-//                instrumenter.insertedInsts.forEach { it.parent?.remove(it) }
-//            }
-//            writeClass(`class`, classFileName)
-//        }
-//    }
-//    log.info("Results:")
-//    val cm = CoverageManager
-//    for (`class` in CM.getConcreteClasses()) {
-//        for ((_, method) in `class`.methods) {
-//            if (!method.isAbstract() && method.name != "<init>" && method.name != "<clinit>") {
-//                if (cm.isFullCovered(method))
-//                    log.info("\"$method\" full covered")
-//                else if (cm.isBodyCovered(method))
-//                    log.info("\"$method\" body covered")
-//                else
-//                    log.info("\"$method\" is not covered")
-//            }
-//        }
-//    }
+    val runner = config.getBooleanValue("runner", false)
+    if (runner) {
+        for (`class` in CM.getConcreteClasses()) {
+            for ((_, method) in `class`.methods) {
+                val classFileName = "${target.canonicalPath}/${`class`.getFullname()}.class"
+                if (!method.isAbstract() && method.name != "<init>" && method.name != "<clinit>") {
+                    val instrumenter = TraceInstrumenter(method)
+                    instrumenter.visit()
+                    writeClass(`class`, classFileName)
+                    val loader = URLClassLoader(arrayOf(target.toURI().toURL()))
+                    CoverageRunner(method, loader).run()
+                    instrumenter.insertedInsts.forEach { it.parent?.remove(it) }
+                }
+                writeClass(`class`, classFileName)
+            }
+        }
+        log.info("Results:")
+        val cm = CoverageManager
+        for (`class` in CM.getConcreteClasses()) {
+            for ((_, method) in `class`.methods) {
+                if (!method.isAbstract() && method.name != "<init>" && method.name != "<clinit>") {
+                    if (cm.isFullCovered(method))
+                        log.info("\"$method\" full covered")
+                    else if (cm.isBodyCovered(method))
+                        log.info("\"$method\" body covered")
+                    else
+                        log.info("\"$method\" is not covered")
+                }
+            }
+        }
+    }
 
     for (`class` in CM.getConcreteClasses()) {
         for ((_, method) in `class`.methods) {
