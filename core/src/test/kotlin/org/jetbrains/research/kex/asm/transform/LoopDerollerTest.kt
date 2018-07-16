6229b7da8249c92c69297306443e361da5787216
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-07-16 19:08:51 +0300

    bugfixes + simple loop test

---
+++ core/src/test/kotlin/org/jetbrains/research/kex/asm/transform/LoopDerollerTest.kt
@@ -0,0 +1,46 @@
+package org.jetbrains.research.kex.asm.transform
+
+import org.jetbrains.research.kex.KexTest
+import org.jetbrains.research.kfg.CM
+import org.jetbrains.research.kfg.analysis.IRVerifier
+import org.jetbrains.research.kfg.analysis.LoopAnalysis
+import org.jetbrains.research.kfg.analysis.LoopSimplifier
+import org.jetbrains.research.kfg.ir.ConcreteClass
+import org.jetbrains.research.kfg.ir.viewCfg
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class LoopDerollerTest : KexTest() {
+    val className = "${packageName}/LoopTests"
+    val `class` = CM.getByName(className)
+
+    init {
+        assert(`class` is ConcreteClass) { log.error("Could not load class `$className`") }
+    }
+
+    @Test
+    fun loopTest() {
+        for ((_, method) in `class`.methods) {
+            if (method.isAbstract()) continue
+
+            val la = LoopAnalysis(method)
+            la.visit()
+            if (la.loops.isEmpty()) continue
+
+            LoopSimplifier(method).visit()
+            la.visit()
+            la.loops.forEach {
+                assertTrue { it.hasSinglePreheader() }
+                assertTrue { it.hasSingleLatch() }
+            }
+
+            val deroller = LoopDeroller(method)
+            deroller.visit()
+
+            la.visit()
+            assertTrue(la.loops.isEmpty())
+
+            IRVerifier(method).visit()
+        }
+    }
+}
---
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/loops.kt
@@ -0,0 +1,95 @@
+package org.jetbrains.research.kex.test
+
+class LoopTests {
+    fun simpleLoop() {
+        var i = 0
+        val loop = arrayOf(1, 2, 123, 123, 123, 324)
+        if (loop.size > 0) println("has elements")
+        else println("empty")
+        while (i < 5) {
+            println(loop[i])
+            ++i
+        }
+    }
+
+    fun simpleLoop(size: Int) {
+        var i = 0
+        val loop = arrayOf(1, 2, 123, 123, 123, 324)
+        if (loop.size > 0) println("has elements")
+        else println("empty")
+        if (loop.size >= size) {
+            println("Index out of bounds")
+            return
+        }
+        while (i < size) {
+            println(loop[i])
+            ++i
+        }
+    }
+
+    fun simpleInnerLoop() {
+        val loop = arrayOf(1, 2, 3, 4, 5, 6, 7)
+        for (i in 0..5) {
+            for (j in i..6) {
+                for (k in i..6) {
+                    if (loop[k] < loop[i]) {
+                        val temp = loop[i]
+                        loop[i] = loop[k]
+                        loop[j] = temp
+                    }
+                }
+            }
+        }
+    }
+
+    fun loopBreak() {
+        val loop = arrayOf(1, 2, 123, 123, 123, 324)
+        for (it in loop) {
+            println(it)
+            if (it == 10) break
+        }
+    }
+
+    fun loopContinue() {
+        val loop = arrayOf(1, 2, 123, 123, 123, 324)
+        var i = 0
+        while (i < 5) {
+            if (loop[i] == 10) continue
+            println(loop[i])
+            ++i
+        }
+    }
+
+    fun loopAll() {
+        val loop = arrayOf(1, 2, 123, 123, 123, 324)
+        var i = 0
+        while (i < loop.size) {
+            if (loop[i] == 10) continue
+            println(loop[i])
+
+            val loop2 = arrayOf(1, 2, 123, 123, 123, 324)
+            var i2 = 0
+            while (i2 < i) {
+                if (loop2[i2] == 10) continue
+                println(loop2[i2])
+
+                val loop3 = arrayOf(1, 2, 123, 123, 123, 324)
+                for (it in loop3) {
+                    println(it)
+                    if (it == 10) break
+                }
+                ++i2
+            }
+            ++i
+
+
+            var i3 = 0
+            while (i3 < loop.size) {
+                if (loop[i3] == 10) continue
+                println(loop[i])
+                ++i3
+            }
+            ++i
+        }
+    }
+}
