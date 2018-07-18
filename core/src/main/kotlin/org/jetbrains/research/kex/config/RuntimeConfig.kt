f3cb2c262f42ad4785ea29ad9cf6ae4523f4e2dd
Azat Abdullin <azat.aam@gmail.com>
2018-07-18 16:10:38 +0300

    runtime config

---
+++ core/src/main/kotlin/org/jetbrains/research/kex/config/RuntimeConfig.kt
@@ -0,0 +1,19 @@
+package org.jetbrains.research.kex.config
+
+object RuntimeConfig : Config {
+    private val options = mutableMapOf<String, MutableMap<String, String>>()
+
+    fun setValue(section: String, name: String, value: String) =
+            options.getOrPut(section) { mutableMapOf() }.set(name, value)
+
+    fun setValue(section: String, name: String, value: Int) =
+            options.getOrPut(section) { mutableMapOf() }.set(name, value.toString())
+
+    fun setValue(section: String, name: String, value: Double) =
+            options.getOrPut(section) { mutableMapOf() }.set(name, value.toString())
+
+    fun setValue(section: String, name: String, value: Boolean) =
+            options.getOrPut(section) { mutableMapOf() }.set(name, value.toString())
+
+    override fun getStringValue(section: String, name: String) = options[section]?.get(name)
+}
