15aa60c9cc1a910fe12b8ff430a08909525b9489
Azat Abdullin <azat.aam@gmail.com>
2018-09-07 12:08:47 +0300

    m

--- core/src/main/kotlin/org/jetbrains/research/kex/asm/analysis/MethodChecker.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/asm/analysis/MethodChecker.kt
@@ -3,7 +3,6 @@ package org.jetbrains.research.kex.asm.analysis
 import org.jetbrains.research.kex.asm.state.PredicateStateAnalysis
 import org.jetbrains.research.kex.smt.Checker
 import org.jetbrains.research.kex.smt.Result
-import org.jetbrains.research.kex.smt.model.ModelRecoverer
 import org.jetbrains.research.kex.util.debug
 import org.jetbrains.research.kex.util.log
 import org.jetbrains.research.kfg.ir.Method
@@ -37,8 +36,8 @@ class MethodChecker(val loader: ClassLoader) : MethodVisitor {
         log.debug(result)
         if (result is Result.SatResult) {
             log.debug(result.model)
-            val recoverer = ModelRecoverer(method, result.model, loader)
-            recoverer.apply()
+//            val recoverer = ModelRecoverer(method, result.model, loader)
+//            recoverer.apply()
 //            log.debug("Recovered: ")
 //            for ((term, value) in recoverer.terms) {
 //                val memspace = if (term.type is KexPointer) "<${term.memspace}>"  else ""
