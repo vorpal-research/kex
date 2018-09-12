1cdb868c8344980814278bdfac1e4c32b803c486
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2018-09-12 15:22:05 +0300

    deroller fix

--- core/src/main/kotlin/org/jetbrains/research/kex/asm/transform/LoopDeroller.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/asm/transform/LoopDeroller.kt
@@ -10,6 +10,7 @@ import org.jetbrains.research.kfg.analysis.LoopVisitor
 import org.jetbrains.research.kfg.ir.BasicBlock
 import org.jetbrains.research.kfg.ir.BodyBlock
 import org.jetbrains.research.kfg.ir.CatchBlock
+import org.jetbrains.research.kfg.ir.Method
 import org.jetbrains.research.kfg.ir.value.BlockUser
 import org.jetbrains.research.kfg.ir.value.IntConstant
 import org.jetbrains.research.kfg.ir.value.Value
@@ -20,215 +21,142 @@ import kotlin.math.abs
 private val derollCount = GlobalConfig.getIntValue("loop", "deroll-count", 3)
 
 object LoopDeroller : LoopVisitor {
+
+    private data class State(
+            val header: BasicBlock,
+            val latch: BasicBlock,
+            val terminatingBlock: BasicBlock,
+            val continueOnTrue: Boolean,
+            var lastHeader: BasicBlock,
+            var lastLatch: BasicBlock,
+            val blockMappings: MutableMap<BasicBlock, BasicBlock>,
+            val instMappings: MutableMap<Value, Value>
+    ) {
+        companion object {
+            fun createState(loop: Loop): State {
+                val terminatingBlock = run {
+                    var current = loop.latch.successors.first()
+                    while (current.terminator is JumpInst) current = current.successors.first()
+                    current
+                }
+                val continueOnTrue = when {
+                    terminatingBlock.terminator.successors.isEmpty() -> false
+                    terminatingBlock.terminator.successors.size == 1 -> false
+                    else -> terminatingBlock.terminator.successors.first() in loop
+                }
+
+                val state = State(loop.header, loop.latch, terminatingBlock,
+                        continueOnTrue, loop.header, loop.preheader,
+                        hashMapOf(), hashMapOf())
+                loop.body.flatten().forEach { state[it] = it }
+
+                return state
+            }
+        }
+
+        operator fun set(block: BasicBlock, value: BasicBlock) {
+            blockMappings[block] = value
+        }
+
+        operator fun set(value: Value, newValue: Value) {
+            instMappings[value] = newValue
+        }
+
+        operator fun get(block: BasicBlock) = blockMappings.getValue(block)
+        operator fun get(value: Value) = instMappings.getValue(value)
+
+        fun getOrDefault(block: BasicBlock, default: BasicBlock) = blockMappings.getOrDefault(block, default)
+        fun getOrDefault(value: Value, default: Value) = instMappings.getOrDefault(value, default)
+    }
+
+
     override fun cleanup() {}
+
+    override fun preservesLoopInfo() = false
 
     override fun visit(loop: Loop) {
         super.visit(loop)
 
         val method = loop.method ?: unreachable { log.error("Can't get method of loop") }
-        val header = loop.header
-        val preheader = loop.preheader
-        val latch = loop.latch
-
-        val terminator = run {
-            var current = latch.successors.first()
-            while (current.terminator is JumpInst) current = current.successors.first()
-            current.terminator
-        }
-        val (continueOnTrue, loopExit) = when {
-            terminator.successors.isEmpty() -> false to null
-            terminator.successors.size == 1 -> false to null
-            else -> {
-                val con = terminator.successors.first() in loop
-                con to terminator.successors[con.toInt()]
-            }
+        val blockOrder = getBlockOrder(loop)
+        val state = State.createState(loop)
+        val derollCount = getDerollCount(loop)
+        val body = loop.body.toMutableList().apply {
+            forEach { loop.removeBlock(it) }
         }
 
-        loop.body.mapNotNull { it as? CatchBlock }.forEach { cb -> cb.getAllPredecessors().forEach { it.addSuccessor(cb) } }
-        val (order, _) = TopologicalSorter(loop.body).sort(header)
-        loop.body.mapNotNull { it as? CatchBlock }.forEach { cb -> cb.getAllPredecessors().forEach { it.removeSuccessor(cb) } }
-        val blockOrder = order.filter { it in loop.body }.reversed()
+        log.debug("Method $method, unrolling loop $loop to $derollCount iterations")
 
-        val currentInsts = hashMapOf<Value, Value>()
-        loop.body.flatten().forEach { currentInsts[it] = it }
-
-        var lastLatch = preheader
-        var lastHeader = header
-
-        val tripCount = getConstantTripCount(loop)
-
-        val body = loop.body.toTypedArray()
-        body.forEach { loop.removeBlock(it) }
-
-        val loopThrowers = method.catchEntries.map { catch ->
-            catch to body.filter { it.handlers.contains(catch) }
-        }.toMap()
-
-        val actualDerollCount = when {
-            tripCount > 0 -> tripCount
-            else -> derollCount
-        }
-        log.debug("Method $method, unrolling loop $loop to $actualDerollCount iterations")
-
-        val methodPhiMappings = hashMapOf<PhiInst, MutableMap<BasicBlock, Value>>()
         val methodPhis = method.filter { it !in body }.flatten().mapNotNull { it as? PhiInst }
-        for (phi in methodPhis) {
-            val incomings = phi.incomings.toMutableMap()
-            body.forEach { incomings.remove(it) }
-            methodPhiMappings[phi] = incomings
-        }
+        val methodPhiMappings = methodPhis.map { phi ->
+            phi to phi.incomings.filterNot { it.key in body }.toMutableMap()
+        }.toMap().toMutableMap()
 
-        for (iteration in 0 until actualDerollCount) {
-            val currentBlocks = hashMapOf<BasicBlock, BasicBlock>()
-            for (block in body) {
-                val newBlock = when (block) {
-                    is BodyBlock -> BodyBlock("${block.name.name}.deroll")
-                    is CatchBlock -> CatchBlock("${block.name.name}.deroll", block.exception)
-                    else -> unreachable { log.error("Unknown block type: ${block.name}") }
-                }
-                currentBlocks[block] = newBlock
-            }
-
-            for ((original, derolled) in currentBlocks) {
-                when (original) {
-                    header -> {
-                        lastLatch.replaceUsesOf(lastHeader, derolled)
-                        derolled.addPredecessor(lastLatch)
-                    }
-                    else -> {
-                        val predecessors = original.predecessors.map { currentBlocks.getValue(it) }
-                        derolled.addPredecessors(*predecessors.toTypedArray())
-                        predecessors.forEach { it.addSuccessor(derolled) }
-                    }
-                }
-                if (original is CatchBlock) {
-                    val throwers = original.throwers.map { currentBlocks.getValue(it) }
-                    derolled as CatchBlock
-                    derolled.addThrowers(throwers)
-                    throwers.forEach { it.addHandler(derolled) }
-                }
-                for (handle in original.handlers) {
-                    val derolledHandle = (currentBlocks[handle] ?: handle) as CatchBlock
-                    derolled.addHandler(derolledHandle)
-                    derolledHandle.addThrowers(listOf(derolled))
-                }
-
-                val successors = original.successors.map {
-                    currentBlocks[it] ?: it
-                }
-                derolled.addSuccessors(*successors.toTypedArray())
-                successors.forEach { it.addPredecessor(derolled) }
+        for (iteration in 0..derollCount) {
+            state.blockMappings.clear()
+            for (block in blockOrder) {
+                val copy = copyBlock(block)
+                state[block] = copy
+                loop.parent?.addBlock(copy)
             }
 
             for (block in blockOrder) {
-                val newBlock = currentBlocks.getValue(block)
-                for (inst in block) {
-                    val updated = inst.update(currentInsts)
-                    updated.location = inst.location
-                    currentInsts[inst] = updated
-                    newBlock += updated
+                copyBlockConnections(state, block)
+            }
 
-                    if (updated is BlockUser) {
-                        currentBlocks.forEach { (original, derolled) -> updated.replaceUsesOf(original, derolled) }
-                    }
-
-                    if (updated is PhiInst && block == header) {
-                        val map = mapOf(currentBlocks.getValue(latch) to lastLatch)
-                        val previousMap = updated.incomings
-                        map.forEach { o, t -> updated.replaceUsesOf(o, t) }
-
-                        if (updated.predecessors.toSet().size == 1) {
-                            val actual = previousMap.getValue(lastLatch)
-                            updated.replaceAllUsesWith(actual)
-                            currentInsts[inst] = actual
-                        }
-                    }
-                }
+            for (block in blockOrder) {
+                copyBlockInstructions(state, block)
 
                 for (phi in methodPhis) {
                     if (phi.incomings.contains(block)) {
                         val value = phi.incomings[block]!!
-                        methodPhiMappings[phi]?.put(currentBlocks.getValue(block), currentInsts.getOrDefault(value, value))
+                        methodPhiMappings[phi]?.put(state[block], state.getOrDefault(value, value))
                     }
                 }
             }
 
-            lastLatch = currentBlocks.getValue(latch)
-            lastHeader = currentBlocks.getValue(header)
+            state.lastLatch = state[state.latch]
+            state.lastHeader = state[state.header]
 
-            currentBlocks.forEach { loop.addBlock(it.value) }
+            for (block in blockOrder) {
+                val mapping = state[block]
+                method.addBefore(state.header, mapping)
+                if (mapping is CatchBlock) method.addCatchBlock(mapping)
+            }
 
             val phiMappings = blockOrder
                     .flatten()
-                    .zip(blockOrder.map { currentBlocks.getValue(it) }.flatten())
+                    .zip(blockOrder.map { state[it] }.flatten())
                     .filter { it.first is PhiInst }
-
-            blockOrder.forEach {
-                val mapping = currentBlocks.getValue(it)
-                method.addBefore(header, mapping)
-                if (mapping is CatchBlock) method.addCatchBlock(mapping)
-            }
-            for ((_, derolled) in currentBlocks) loop.parent?.addBlock(derolled)
-
-            for ((orig, new) in phiMappings) {
-                if (new is PhiInst) {
-                    val bb = new.parent!!
-                    val incomings = new.incomings.filter { it.key in bb.predecessors }
-                    val phiValue = when {
-                        incomings.size == 1 -> {
-                            bb -= new
-                            incomings.values.first()
-                        }
-                        else -> {
-                            val newPhi = IF.getPhi(new.type, incomings)
-                            bb.replace(new, newPhi)
-                            newPhi
-                        }
-                    }
-                    currentInsts[orig] = phiValue
-                    new.replaceAllUsesWith(phiValue)
-                }
-            }
+            remapBlockPhis(state, phiMappings)
         }
 
-        if (loopExit != null) {
-            lastLatch.replaceUsesOf(lastHeader, loopExit)
-            loopExit.addPredecessor(lastLatch)
-        }
         val unreachableBlock = BodyBlock("unreachable")
         unreachableBlock.add(IF.getUnreachable())
-        if (lastLatch.terminator.successors.size == 1) {
-            lastLatch.replaceUsesOf(loopExit!!, unreachableBlock)
-        } else {
-            val block = lastLatch.terminator.successors[(!continueOnTrue).toInt()]
-            lastLatch.replaceUsesOf(block, unreachableBlock)
-        }
         method.add(unreachableBlock)
 
-        for (block in body) {
-            block.predecessors.toTypedArray().forEach { block.removePredecessor(it) }
-            block.successors.toTypedArray().forEach { block.removeSuccessor(it) }
-            method.remove(block)
+        val lastTerminator = state[state.terminatingBlock]
+        val continueBlock = lastTerminator.terminator.successors[(!state.continueOnTrue).toInt()]
+        lastTerminator.replaceUsesOf(continueBlock, unreachableBlock)
+        for (block in blockOrder.reversed()) {
+            if (block == state.terminatingBlock) break
+            val copy = state[block]
+            copy.predecessors.toTypedArray().forEach { it.removeSuccessor(copy) }
+            copy.successors.toTypedArray().forEach { it.removePredecessor(copy) }
+            method.remove(copy)
         }
 
-        for ((catch, throwers) in loopThrowers) {
-            throwers.forEach {
-                catch.removeThrower(it)
-                it.removeHandler(catch)
-            }
-        }
-        for (phi in methodPhis) {
-            val newPhi = IF.getPhi(phi.type, methodPhiMappings.getValue(phi))
-            newPhi.location = phi.location
-
-            val bb = phi.parent!!
-            bb.insertBefore(phi, newPhi)
-            phi.replaceAllUsesWith(newPhi)
-            bb -= phi
-        }
+        cleanupBody(method, body)
+        remapMethodPhis(methodPhis, methodPhiMappings)
     }
 
-    override fun preservesLoopInfo() = false
+    private fun getDerollCount(loop: Loop): Int {
+        val tripCount = getConstantTripCount(loop)
+        return when {
+            tripCount > 0 -> tripCount
+            else -> derollCount
+        }
+    }
 
     private fun getConstantTripCount(loop: Loop): Int {
         val header = loop.header
@@ -246,6 +174,9 @@ object LoopDeroller : LoopVisitor {
         val (init, updated) = when (value) {
             is PhiInst -> {
                 val incomings = value.incomings
+                if (incomings.size != 2) {
+                    println("wait")
+                }
                 require(incomings.size == 2) { log.error("Unexpected number of header incomings") }
                 incomings.getValue(preheader) to incomings.getValue(latch)
             }
@@ -272,5 +203,130 @@ object LoopDeroller : LoopVisitor {
         }
 
         return abs((max.value - init.value) / update.value)
+    }
+
+    private fun getBlockOrder(loop: Loop): List<BasicBlock> {
+        val header = loop.header
+        loop.body.mapNotNull { it as? CatchBlock }.forEach { cb -> cb.getAllPredecessors().forEach { it.addSuccessor(cb) } }
+        val (order, _) = TopologicalSorter(loop.body).sort(header)
+        loop.body.mapNotNull { it as? CatchBlock }.forEach { cb -> cb.getAllPredecessors().forEach { it.removeSuccessor(cb) } }
+        return order.filter { it in loop.body }.reversed()
+    }
+
+    private fun copyBlock(block: BasicBlock) = when (block) {
+        is BodyBlock -> BodyBlock("${block.name.name}.deroll")
+        is CatchBlock -> CatchBlock("${block.name.name}.deroll", block.exception)
+        else -> unreachable { log.error("Unknown block type: ${block.name}") }
+    }
+
+    private fun copyBlockConnections(state: State, original: BasicBlock) {
+        val derolled = state[original]
+        when (original) {
+            state.header -> {
+                state.lastLatch.replaceUsesOf(state.lastHeader, derolled)
+                derolled.addPredecessor(state.lastLatch)
+            }
+            else -> {
+                val predecessors = original.predecessors.map { state[it] }
+                derolled.addPredecessors(*predecessors.toTypedArray())
+                predecessors.forEach { it.addSuccessor(derolled) }
+            }
+        }
+        if (original is CatchBlock) {
+            val throwers = original.throwers.map { state[it] }
+            derolled as CatchBlock
+            derolled.addThrowers(throwers)
+            throwers.forEach { it.addHandler(derolled) }
+        }
+        for (handle in original.handlers) {
+            val derolledHandle = (state.getOrDefault(handle, handle)) as CatchBlock
+            derolled.addHandler(derolledHandle)
+            derolledHandle.addThrowers(listOf(derolled))
+        }
+
+        val successors = original.successors.map {
+            state.getOrDefault(it, it)
+        }
+        derolled.addSuccessors(*successors.toTypedArray())
+        successors.forEach { it.addPredecessor(derolled) }
+    }
+
+    private fun copyBlockInstructions(state: State, original: BasicBlock) {
+        val newBlock = state[original]
+        for (inst in original) {
+            val updated = inst.update(state.instMappings)
+            updated.location = inst.location
+            state[inst] = updated
+            newBlock += updated
+
+            if (updated is BlockUser) {
+                state.blockMappings.forEach { (original, derolled) -> updated.replaceUsesOf(original, derolled) }
+            }
+
+            if (updated is PhiInst && original == state.header) {
+                val map = mapOf(state[state.latch] to state.lastLatch)
+                val previousMap = updated.incomings
+                map.forEach { o, t -> updated.replaceUsesOf(o, t) }
+
+                if (updated.predecessors.toSet().size == 1) {
+                    val actual = previousMap.getValue(state.lastLatch)
+                    updated.replaceAllUsesWith(actual)
+                    state[inst] = actual
+                }
+            }
+        }
+    }
+
+    private fun remapBlockPhis(state: State, phiMappings: List<Pair<Instruction, Instruction>>) {
+        for ((orig, new) in phiMappings) {
+            if (new is PhiInst) {
+                val bb = new.parent!!
+                val incomings = new.incomings.filter { it.key in bb.predecessors }
+                val phiValue = when {
+                    incomings.size == 1 -> {
+                        bb -= new
+                        incomings.values.first()
+                    }
+                    else -> {
+                        val newPhi = IF.getPhi(new.type, incomings)
+                        bb.replace(new, newPhi)
+                        newPhi
+                    }
+                }
+                state[orig] = phiValue
+                new.replaceAllUsesWith(phiValue)
+            }
+        }
+    }
+
+    private fun remapMethodPhis(phis: List<PhiInst>, newMappings: Map<PhiInst, Map<BasicBlock, Value>>) {
+        for (phi in phis) {
+            val newPhi = IF.getPhi(phi.type, newMappings.getValue(phi))
+            newPhi.location = phi.location
+
+            val bb = phi.parent!!
+            bb.insertBefore(phi, newPhi)
+            phi.replaceAllUsesWith(newPhi)
+            bb -= phi
+        }
+    }
+
+    private fun cleanupBody(method: Method, body: List<BasicBlock>) {
+        for (block in body) {
+            block.predecessors.toTypedArray().forEach { block.removePredecessor(it) }
+            block.successors.toTypedArray().forEach { block.removeSuccessor(it) }
+            method.remove(block)
+        }
+
+        val loopThrowers = method.catchEntries.map { catch ->
+            catch to body.filter { it.handlers.contains(catch) }
+        }.toMap()
+
+        for ((catch, throwers) in loopThrowers) {
+            throwers.forEach {
+                catch.removeThrower(it)
+                it.removeHandler(catch)
+            }
+        }
     }
 }
