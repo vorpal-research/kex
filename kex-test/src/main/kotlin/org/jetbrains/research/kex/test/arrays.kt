4d0cdbd14cc11211ae45c6cc291f24c3a1f20c45
Azat Abdullin <azat.aam@gmail.com>
2018-09-12 15:22:05 +0300

    deroller fix

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/arrays.kt
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/arrays.kt
@@ -19,28 +19,13 @@ class ArrayTests {
 
     fun testArrayWrite() {
         val emptyArray = intArrayOf(0, 0, 0, 0, 0)
-        val size = emptyArray.size
-        for (i in 0 until size) {
-            emptyArray[i] = 5 * 5
+
+        for (i in 0 until 5) {
+            emptyArray[i] = i * i
         }
 
-//        Intrinsics.assertReachable()
-//        if (emptyArray[0] == 10)
-//            Intrinsics.assertReachable()
-//        if (emptyArray[1] == 20)
-//            Intrinsics.assertReachable()
-//        if (emptyArray[2] == 30)
-//            Intrinsics.assertReachable()
-//        if (emptyArray[3] == 40)
-//            Intrinsics.assertReachable()
-//        if (emptyArray[4] == 50)
-//            Intrinsics.assertReachable()
-//        emptyArray[4] = 15
-//        for (i in 0 until 1) {
-//            val element = emptyArray[i]
-//            if (element == 5 * 5)
-//                Intrinsics.assertReachable()
-//        }
-//        Intrinsics.assertReachable(emptyArray[4] == 15)
+        for (i in 0 until 5) {
+            Intrinsics.assertReachable(emptyArray[i] == i * i)
+        }
     }
 }
