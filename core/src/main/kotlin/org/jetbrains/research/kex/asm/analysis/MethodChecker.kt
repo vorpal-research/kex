4a9f5d1a1c215e06a5c7ff0f54789f91861508a7
Azat Abdullin <azat.aam@gmail.com>
2018-10-26 16:03:18 +0300

    method checker tries to check each block of a method separately

--- core/src/main/kotlin/org/jetbrains/research/kex/asm/analysis/MethodChecker.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/asm/analysis/MethodChecker.kt
@@ -1,14 +1,16 @@
 package org.jetbrains.research.kex.asm.analysis
 
 import org.jetbrains.research.kex.asm.state.PredicateStateAnalysis
-import org.jetbrains.research.kex.ktype.KexPointer
+import org.jetbrains.research.kex.asm.state.PredicateStateBuilder
 import org.jetbrains.research.kex.smt.Checker
 import org.jetbrains.research.kex.smt.Result
 import org.jetbrains.research.kex.smt.model.ModelRecoverer
-import org.jetbrains.research.kex.state.transformer.memspace
+import org.jetbrains.research.kex.trace.TraceManager
+import org.jetbrains.research.kex.trace.runner.SimpleRunner
 import org.jetbrains.research.kex.util.debug
 import org.jetbrains.research.kex.util.log
 import org.jetbrains.research.kex.util.tryOrNull
+import org.jetbrains.research.kfg.ir.BasicBlock
 import org.jetbrains.research.kfg.ir.Method
 import org.jetbrains.research.kfg.ir.value.instruction.ReturnInst
 import org.jetbrains.research.kfg.visitor.MethodVisitor
@@ -16,14 +18,15 @@ import org.jetbrains.research.kfg.visitor.MethodVisitor
 val Method.returnInst get() = this.flatten().lastOrNull { it is ReturnInst } as? ReturnInst
 
 class MethodChecker(private val loader: ClassLoader) : MethodVisitor {
-    val psa = PredicateStateAnalysis
+    private val tm = TraceManager
+    private val psa = PredicateStateAnalysis
 
     override fun cleanup() {}
 
     override fun visit(method: Method) {
         super.visit(method)
 
-        if (method.isAbstract) return
+        if (method.isAbstract || method.isConstructor) return
 
         log.run {
             debug(method)
@@ -32,22 +35,41 @@ class MethodChecker(private val loader: ClassLoader) : MethodVisitor {
         }
 
         val psb = psa.builder(method)
+
+        // check body blocks backwards, to reduce number of runs
+        for (block in method.bodyBlocks.reversed()) {
+            if (tm.isCovered(block)) continue
+
+            coverBlock(method, block, psb)
+
+            log.debug("Block ${block.name} is covered = ${tm.isCovered(block)}")
+            log.debug()
+        }
+    }
+
+    private fun coverBlock(method: Method, block: BasicBlock, psb: PredicateStateBuilder) {
         val checker = Checker(method, loader, psb)
 
-        val returnInst = method.returnInst ?: return
-
-        val result = checker.checkReachable(returnInst)
+        log.debug("Checking reachability of ${block.terminator.print()}")
+        val result = checker.checkReachable(block.terminator)
         log.debug(result)
-        if (result is Result.SatResult) {
-            log.debug(result.model)
-            val recoverer = ModelRecoverer(method, result.model, loader)
-            val model = recoverer.apply()
-            log.debug("Recovered: ${tryOrNull { model.toString() }}")
-            for ((term, value) in recoverer.terms) {
-                val memspace = if (term.type is KexPointer) "<${term.memspace}>"  else ""
-                log.debug("$term$memspace = $value")
+
+        when (result) {
+            is Result.SatResult -> {
+                log.debug(result.model)
+                val recoverer = ModelRecoverer(method, result.model, loader)
+                val model = recoverer.apply()
+                log.debug("Recovered: ${tryOrNull { model.toString() }}")
+
+                tryOrNull {
+                    val trace = SimpleRunner(method, loader).invoke(model.instance, model.arguments.toTypedArray())
+                    TraceManager.addTrace(method, trace)
+                }
             }
+            is Result.UnsatResult -> {
+                log.debug("Instruction ${block.terminator.print()} is unreachable")
+            }
+            is Result.UnknownResult -> Unit
         }
-        log.debug()
     }
 }
