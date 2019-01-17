5c79199f84934b001682e0a18351043b30c11d61
Azat Abdullin <azat.aam@gmail.com>
2019-01-17 16:43:49 +0300

    fixes in array recovering

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
@@ -1,13 +1,18 @@
 package org.jetbrains.research.kex.test.debug
 
+import org.jetbrains.research.kex.test.Intrinsics
+
 // add functions here to debug them
 
-class Icfpc2018Test {
-    class Result(val solutions: MutableMap<String, Solution>) {
-        fun getSortedSolutions(): List<Pair<String, Solution>> = TODO()
-    }
-
-    class Solution(val energy: Long, val trace: String) {
-        fun solve() {}
+fun testSimple(array: IntArray) {
+    var count = 0
+    if (array.size < 4) throw IllegalArgumentException()
+    if (array[0] == 1) ++count
+    if (array[1] == 2) ++count
+    if (array[2] == 3) ++count
+    if (array[3] == 4) ++count
+    if (count >= 3) {
+        println("Yes")
+        Intrinsics.assertReachable()
     }
 }
