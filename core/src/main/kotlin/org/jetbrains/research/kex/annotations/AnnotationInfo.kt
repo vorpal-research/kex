a47d1cd7b9322ab72effc938ecb5f227237cc28b
Azat Abdullin <azat.aam@gmail.com>
2019-07-31 17:05:27 +0300

    annotations now use builders, removed custom expressions

--- core/src/main/kotlin/org/jetbrains/research/kex/annotations/AnnotationInfo.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/annotations/AnnotationInfo.kt
@@ -1,14 +1,8 @@
 package org.jetbrains.research.kex.annotations
 
-import org.jetbrains.research.kex.state.BasicState
-import org.jetbrains.research.kex.state.ChainState
-import org.jetbrains.research.kex.state.ChoiceState
 import org.jetbrains.research.kex.state.PredicateState
 import org.jetbrains.research.kex.state.predicate.CallPredicate
-import org.jetbrains.research.kex.state.predicate.Predicate
-import org.jetbrains.research.kex.state.predicate.PredicateFactory
 import org.jetbrains.research.kex.state.term.Term
-import org.jetbrains.research.kex.state.term.TermFactory
 import kotlin.reflect.full.findAnnotation
 
 abstract class AnnotationInfo {
@@ -29,27 +23,6 @@ abstract class AnnotationInfo {
                     "\"${AnnotationFunctionality::class.qualifiedName}\"")
         annotation.name
     }
-
-    protected inline val pf get() = PredicateFactory
-    protected inline val tf get() = TermFactory
-
-    fun single(predicate: Predicate): PredicateState = BasicState(listOf(predicate))
-    inline fun single(lambda: () -> Predicate): PredicateState = single(lambda())
-
-    inline fun assume(lambda: PredicateExpression.() -> Predicate): PredicateState =
-            single(PredicateExpression.Assume.lambda())
-    inline fun state(lambda: PredicateExpression.() -> Predicate): PredicateState =
-            single(PredicateExpression.State.lambda())
-    inline fun path(lambda: PredicateExpression.() -> Predicate): PredicateState =
-            single(PredicateExpression.Path.lambda())
-
-    inline fun term(lambda: TermExpression.() -> Term) = TermExpression.lambda()
-
-    inline fun basic(lambda: ExpressionBuilder.() -> Unit) =
-            BasicState(ExpressionBuilder.build(lambda))
-    fun chain(first: PredicateState, second: PredicateState) = ChainState(first, second)
-    fun choice(states: List<PredicateState>) = ChoiceState(states)
-    fun choice(vararg states: PredicateState) = ChoiceState(states.toList())
 
     override fun toString() = name
 }
--- core/src/main/kotlin/org/jetbrains/research/kex/annotations/JetBrainsAnnotations.kt
+++ core/src/main/kotlin/org/jetbrains/research/kex/annotations/JetBrainsAnnotations.kt
@@ -1,23 +1,29 @@
 package org.jetbrains.research.kex.annotations
 
 import org.jetbrains.research.kex.asm.manager.MethodManager
-import org.jetbrains.research.kex.config.GlobalConfig
 import org.jetbrains.research.kex.config.kexConfig
 import org.jetbrains.research.kex.ktype.KexBool
 import org.jetbrains.research.kex.state.PredicateState
+import org.jetbrains.research.kex.state.StateBuilder
 import org.jetbrains.research.kex.state.predicate.CallPredicate
+import org.jetbrains.research.kex.state.predicate.assume
+import org.jetbrains.research.kex.state.predicate.path
+import org.jetbrains.research.kex.state.predicate.state
 import org.jetbrains.research.kex.state.term.CallTerm
 import org.jetbrains.research.kex.state.term.Term
+import org.jetbrains.research.kex.state.term.term
+import org.jetbrains.research.kex.state.wrap
 import org.jetbrains.research.kex.util.unreachable
 
 @AnnotationFunctionality("org.jetbrains.annotations.Range")
 class Range(val from: Long, val to: Long) : AnnotationInfo() {
-    override fun preciseValue(value: Term) = assume { (from..to has value) equality true }
+    override fun preciseValue(value: Term) =
+            assume { (value ge from) equality true }.wrap() + assume { (value le to) equality true }
 }
 
 @AnnotationFunctionality("org.jetbrains.annotations.NotNull")
 class NotNull : AnnotationInfo() {
-    override fun preciseValue(value: Term) = assume { value inequality null }
+    override fun preciseValue(value: Term) = assume { value inequality null }.wrap()
 }
 
 @AnnotationFunctionality("org.jetbrains.annotations.Nullable")
@@ -27,9 +33,9 @@ class Nullable : AnnotationInfo()
 class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationInfo() {
 
     private enum class Constraints(val literal: String) {
-        Any("_"),     Null("null"),   NotNull("!null"),
+        Any("_"), Null("null"), NotNull("!null"),
         True("true"), False("false"), Fail("fail"),
-        New("new"),   This("this"),   Param("param");
+        New("new"), This("this"), Param("param");
 
         companion object {
             private val byLiteral = values().associateBy { it.literal }
@@ -39,6 +45,7 @@ class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationIn
     }
 
     private class Record(val params: List<Constraints>, val result: Constraints, val meta: Int = -1)
+
     private val records = mutableListOf<Record>()
 
     override fun initialize(n: Int) {
@@ -50,8 +57,9 @@ class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationIn
                     argsStr.isBlank() -> emptyList()
                     else -> argsStr.split(',').map { Constraints[it.trim()] }
                 }
-                check(args.size == paramN) { "Parameters count ${args.size} in contract requires to be the " +
-                        "same as in the call $paramN" }
+                check(args.size == paramN) {
+                    "Parameters count ${args.size} in contract requires to be the same as in the call $paramN"
+                }
                 val resultLiteral = result.trim()
                 records += when {
                     resultLiteral.startsWith("param") -> {
@@ -70,7 +78,7 @@ class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationIn
                 when (param) {
                     in effects2, Constraints.New, Constraints.Fail ->
                         throw IllegalStateException("Constraint ${param.literal} is an effect")
-                    else -> {}
+                    else -> Unit
                 }
             }
         }
@@ -89,20 +97,19 @@ class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationIn
         }
     }
 
-    override fun preciseBeforeCall(predicate: CallPredicate) = basic {
-        // Find fail situations
+    override fun preciseBeforeCall(predicate: CallPredicate): PredicateState? {
+        val builder = StateBuilder()
         val call = predicate.call as CallTerm
         val args = call.arguments
         for (record in records.asSequence().filter { it.result == Constraints.Fail }) {
             val params = record.params
-            var accumulator: Term = const(true)
             for (i in 0 until params.size) {
                 if (params[i] == Constraints.Any)
                     continue
-                accumulator = accumulator and getTermByConstraint(params[i], args[i])
+                builder += assume { getTermByConstraint(params[i], args[i]) equality false }
             }
-            assumeFalse { accumulator }
         }
+        return builder.apply()
     }
 
     private companion object {
@@ -117,81 +124,85 @@ class Contract(val value: String = ""/*, pure: Boolean = false*/) : AnnotationIn
         val args = call.arguments
         val returnTerm = predicate.lhvUnsafe ?: return null
         val id = id.toString()
-        var result: PredicateState = basic {  }
+        val result = StateBuilder()
 
         val inlineEnabled = kexConfig.getBooleanValue("smt", "ps-inlining", true)
                 && MethodManager.InlineManager.isInlinable(call.method)
+
         // New statement insertion
         if (!inlineEnabled && records.any { it.result == Constraints.New }) {
             for ((i, record) in records.asSequence().withIndex().filter { it.value.result == Constraints.New }) {
                 val params = record.params
                 val argUnion = term { value(KexBool(), "%contract$id.$i.args") }
-                result += basic {
+                result += state {
                     var accumulator: Term = const(true)
                     for (j in 0 until params.size) {
                         if (params[j] != Constraints.Any)
                             accumulator = accumulator and getTermByConstraint(params[j], args[j])
                     }
-                    argUnion load accumulator
-                } + choice(basic {
-                    path { argUnion equality true }
-                    state { returnTerm.new() }
-                }, basic { path { argUnion equality false } } )
+                    argUnion equality accumulator
+                }
+                result += listOf(
+                        path { argUnion equality true }.wrap() + state { returnTerm.new() },
+                        path { argUnion equality false }.wrap()
+                )
             }
         }
+
         // Make boolean functions for same cases
-        result += basic {
-            if (!records.any { it.result in effects1 })
-                return@basic
-            for (record in records.asSequence().filter {
-                    it.result in effects1 || inlineEnabled && it.result == Constraints.New }) {
+        if (records.all { it.result !in effects1 }) {
+            for (record in records.filter { it.result in effects1 || inlineEnabled && it.result == Constraints.New }) {
                 val params = record.params
-                val effect = when (record.result) {
-                    Constraints.Null -> returnTerm eq const(null)
-                    Constraints.NotNull, Constraints.New -> returnTerm neq const(null)
-                    Constraints.True -> returnTerm eq const(true)
-                    Constraints.False -> returnTerm eq const(false)
-                    else -> unreachable { record.result }
-                }
-                var argsCheck: Term? = null
-                for (i in 0 until params.size) {
-                    if (params[i] == Constraints.Any)
-                        continue
-                    if (argsCheck == null)
-                        argsCheck = getTermByConstraint(params[i], args[i])
+
+                result += assume {
+                    val effect = when (record.result) {
+                        Constraints.Null -> returnTerm eq const(null)
+                        Constraints.NotNull, Constraints.New -> returnTerm neq const(null)
+                        Constraints.True -> returnTerm eq const(true)
+                        Constraints.False -> returnTerm eq const(false)
+                        else -> unreachable { record.result }
+                    }
+                    var argsCheck: Term? = null
+                    for (i in 0 until params.size) {
+                        if (params[i] == Constraints.Any)
+                            continue
+                        if (argsCheck == null)
+                            argsCheck = getTermByConstraint(params[i], args[i])
+                        else
+                            argsCheck = argsCheck and getTermByConstraint(params[i], args[i])
+                    }
+                    val check = if (argsCheck != null)
+                        !argsCheck or effect
                     else
-                        argsCheck = argsCheck and getTermByConstraint(params[i], args[i])
+                        effect
+                    check equality true
                 }
-                val check = if (argsCheck != null)
-                    not(argsCheck) or effect
-                else
-                    effect
-                assumeTrue { check }
             }
         }
         if (records.any { it.result in effects2 }) {
-            for ((i, record) in records.asSequence().withIndex().filter { it.value.result in effects2 }) {
+            for ((i, record) in records.withIndex().filter { it.value.result in effects2 }) {
                 val params = record.params
                 val argUnion = term { value(KexBool(), "%contract$id.$i.args") }
-                result += basic {
+                result += assume {
                     var accumulator: Term = const(true)
                     for (j in 0 until params.size) {
                         if (params[j] != Constraints.Any)
                             accumulator = accumulator and getTermByConstraint(params[j], args[j])
                     }
-                    argUnion load accumulator
-                } + choice(basic {
-                    path { argUnion equality true }
-                    assume {
-                        returnTerm equality when (record.result) {
-                            Constraints.This -> call.owner
-                            Constraints.Param -> args[record.meta]
-                            else -> unreachable { record.result }
-                        }
-                    }
-                }, basic { path { argUnion equality false } } )
+                    argUnion equality accumulator
+                }
+                result += listOf(
+                        path { argUnion equality true }.wrap() + assume {
+                            returnTerm equality when (record.result) {
+                                Constraints.This -> call.owner
+                                Constraints.Param -> args[record.meta]
+                                else -> unreachable { record.result }
+                            }
+                        },
+                        path { argUnion equality false }.wrap()
+                )
             }
         }
-        return result
+        return result.apply()
     }
 }
