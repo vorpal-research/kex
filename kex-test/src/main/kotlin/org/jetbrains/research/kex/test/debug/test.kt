7ae953b750389843ac65e83704f6cea570642562
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2019-08-05 15:25:01 +0300

    more fixes

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
@@ -2,99 +2,22 @@
 
 package org.jetbrains.research.kex.test.debug
 
-class InspectionPositionalArguments(
-        val name: String,
-        val description: String,
-        val minCount: Int?,
-        val maxCount: Int?
-)
+import kotlin.math.atan
+import kotlin.math.sqrt
 
-enum class InspectionArgumentType {
-    Parameter,
-    MultiParameter,
-    Flag
+fun sqr(x: Double) = x * x
+
+data class Point(val x: Double, val y: Double) {
+    fun distance(other: Point): Double = sqrt(sqr(x - other.x) + sqr(y - other.y))
 }
 
-class InspectionArgument(
-        val name: String,
-        val aliasNames: List<String>,
-        val shortNames: List<Char>,
-        val description: String,
-        val type: InspectionArgumentType,
-        val isOptional: Boolean,
-        val defaultValue: String? = null
-)
+data class Segment(val begin: Point, val end: Point) {
+    override fun equals(other: Any?) =
+            other is Segment && (begin == other.begin && end == other.end || end == other.begin && begin == other.end)
 
-class InspectionArguments(
-        val arguments: List<InspectionArgument>,
-        val name: String = "",
-        val description: String = "",
-        val positionalArguments: InspectionPositionalArguments?
-) {
-    fun generateHelpText(): String {
-        var helpString = ""
-        val tab = "  "
+    override fun hashCode() =
+            begin.hashCode() + end.hashCode()
 
-        if (!this.name.isEmpty()) {
-            helpString += "${this.name}\n"
-        }
-
-        if (!this.description.isEmpty())
-            helpString += "${this.description}\n\n"
-
-        this.arguments.forEach { argument ->
-            helpString += tab
-
-            if (argument.isOptional)
-                helpString += "["
-
-            helpString += "--" + argument.name
-
-            argument.aliasNames.forEach { aliasName ->
-                helpString += " | --" + aliasName
-            }
-
-            argument.shortNames.forEach { shortName ->
-                helpString += " | -" + shortName
-            }
-
-            if (argument.isOptional)
-                helpString += "]"
-
-            if (argument.type == InspectionArgumentType.MultiParameter) {
-                helpString += " <value> (repeat parameter for multiple values)"
-            } else if (argument.type == InspectionArgumentType.Parameter) {
-                helpString += " <value>"
-            }
-
-            helpString += "\n"
-
-            if (!argument.description.isEmpty()) {
-                helpString += "$tab$tab${argument.description}\n"
-            }
-        }
-
-        val positionalArgumentsConfig = this.positionalArguments
-
-        if (positionalArgumentsConfig != null) {
-            helpString += "\n"
-            helpString += "$tab<${positionalArgumentsConfig.name}>..."
-
-            if (positionalArgumentsConfig.minCount != null) {
-                helpString += " (min ${positionalArgumentsConfig.minCount})"
-            }
-
-            if (positionalArgumentsConfig.maxCount != null) {
-                helpString += " (max ${positionalArgumentsConfig.maxCount})"
-            }
-
-            helpString += "\n"
-
-            if (!positionalArgumentsConfig.description.isEmpty()) {
-                helpString += "$tab$tab${positionalArgumentsConfig.description}\n"
-            }
-        }
-
-        return helpString
-    }
+    fun middle() = Point((begin.x + end.x) / 2, (begin.y + end.y) / 2)
+    fun angle() = atan((end.y - begin.y) / (end.x - begin.x))
 }
