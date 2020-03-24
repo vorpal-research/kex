43cb30c052db6f3606552a842d85a53fad5a374e
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-03-24 18:25:13 +0300

    kex-test module does not depend from other modules and can be built separately

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
@@ -2,47 +2,35 @@
 
 package org.jetbrains.research.kex.test.debug
 
-import java.io.ByteArrayInputStream
-
 class BasicTests {
-    object AnnotatedMethodsThere {
-        @JvmStatic
-        fun rangeExample(param0: Any?, param1: Int): Int {
-            return param1 + 2
-        }
-        @JvmStatic
-        // "!null, _ -> !null; _, true -> new; _, false -> param1"
-        fun haveContract(param0: Any?, param1: Boolean): Any? = if (param1) Any() else param0
 
-        // "false -> fail; _ -> this"
-        fun assertTrue(param0: Boolean): AnnotatedMethodsThere {
-            if (!param0) throw IllegalStateException()
-            return this
-        }
+    abstract class A
+    abstract class B
+    interface C
 
-        fun assertNotNull(param0: Any?) {
-            if (param0 == null) throw IllegalArgumentException()
-        }
+    open class A1 : A(), C
+    class A2 : A()
+    class A3 : A1()
 
-        fun makeBeautifulList(n: Int) = MutableList(n) { 0 }
-    }
+    class B1 : B()
+    class B2 : B(), C
 
-    class ThatClassContainsHighQualityCodeToProf {
-        fun incredibleMethod(exitingArgument: String, unusualNumber: Double): List<Int> {
-            val importantMethods = AnnotatedMethodsThere
-            val lovelyInteger = unusualNumber.toInt()
-            if (lovelyInteger.toDouble() == unusualNumber) {
-                val bestStream = ByteArrayInputStream(exitingArgument.toByteArray())
-                val meaningfulResult = importantMethods.makeBeautifulList(lovelyInteger)
-                //while (bestStream.available() > 0) {
-                val remarkableLetter = bestStream.read()
-                importantMethods.assertTrue(importantMethods.assertTrue(remarkableLetter > 0) == importantMethods)
-                meaningfulResult += remarkableLetter
-                //}
-                importantMethods.assertNotNull(meaningfulResult)
-                return meaningfulResult
+    fun testTypes(a: A, b: B) {
+        if (a is A1) {
+            if (a is A3) {
+                println("A3")
             }
-            return emptyList()
+            println("A1")
+        }
+        if (a is A2) {
+            println("A2")
+        }
+
+        if (b is B1) {
+            println("B1")
+        }
+        if (b is B2) {
+            println("B2")
         }
     }
 
