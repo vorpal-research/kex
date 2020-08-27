727b8f688f18d7019abb8827167f7254ac538c57
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-08-27 17:05:51 +0300

    fixes for dword memory operations

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/debug/test.kt
@@ -3,33 +3,26 @@
 package org.jetbrains.research.kex.test.debug
 
 class BasicTests {
+//
+//    class Value(val name: String)
+//
+//    class LocalArray() {
+//        val set = hashMapOf<Int, Int>()
+//    }
+//
+//    fun testAbstractClass(array: LocalArray) {
+//        println("a")
+//    }
 
-    class Value(val name: String)
-
-    class LocalArray(private val locals: MutableMap<Int, Value> = hashMapOf()) : MutableMap<Int, Value> by locals {
-    override fun clear() {
-        locals.clear()
+    class Dbl(val value: Long) {
+        override fun toString() = "Dbl(value = $value)"
     }
 
-    override fun put(key: Int, value: Value): Value? {
-        val prev = locals.put(key, value)
-        return prev
-    }
-
-    override fun putAll(from: Map<out Int, Value>) {
-        from.forEach {
-            put(it.key, it.value)
+    fun test(d: Dbl) {
+        if (d.value < 75L) {
+            println(d)
         }
     }
 
-    override fun remove(key: Int): Value? {
-        val res = locals.remove(key)
-        return res
-    }
-}
-
-    fun testAbstractClass(array: LocalArray) {
-        println("a")
-    }
 
 }
