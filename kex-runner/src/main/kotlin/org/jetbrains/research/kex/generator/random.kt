2d5d10629fa64be23bbd434587004e1588e39abb
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-09-01 17:52:15 +0300

    generating only instantiable objects

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/generator/random.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/generator/random.kt
@@ -5,36 +5,44 @@ import com.abdullin.kthelper.logging.debug
 import com.abdullin.kthelper.logging.log
 import org.jetbrains.research.kex.ExecutionContext
 import org.jetbrains.research.kex.asm.state.PredicateStateAnalysis
-import org.jetbrains.research.kex.generator.callstack.CallStack
+import org.jetbrains.research.kex.asm.util.Visibility
+import org.jetbrains.research.kex.asm.util.visibility
+import org.jetbrains.research.kex.config.kexConfig
 import org.jetbrains.research.kex.generator.callstack.CallStackExecutor
 import org.jetbrains.research.kex.generator.callstack.CallStackGenerator
-import org.jetbrains.research.kex.generator.descriptor.Descriptor
-import org.jetbrains.research.kex.generator.descriptor.DescriptorStatistics
 import org.jetbrains.research.kex.generator.descriptor.descriptor
-import org.jetbrains.research.kex.ktype.type
+import org.jetbrains.research.kex.generator.descriptor.isInstantiable
 import org.jetbrains.research.kex.random.Randomizer
 import org.jetbrains.research.kex.util.loadClass
 import org.jetbrains.research.kfg.ClassManager
 import org.jetbrains.research.kfg.Package
 
+private val visibilityLevel by lazy { kexConfig.getEnumValue("apiGeneration", "visibility", true, Visibility.PUBLIC) }
+
 class RandomDescriptorGenerator(val ctx: ExecutionContext, val target: Package, val psa: PredicateStateAnalysis) {
     val random: Randomizer get() = ctx.random
     val cm: ClassManager get() = ctx.cm
 
-    val ClassManager.randomClass get() = this.concreteClasses
-            .filter { it.`package`.isChild(target) }
-            .filterNot { it.isEnum }
-            .random()
+    val ClassManager.randomClass
+        get() = this.concreteClasses
+                .filter { it.`package`.isChild(target) }
+                .filterNot { it.isEnum }
+                .random()
 
-    val Descriptor.callStack: CallStack?
-        get() = `try`<CallStack?> {
-            val cs = CallStackGenerator(ctx, psa).generate(this)
-            DescriptorStatistics.addDescriptor(this, cs)
-            cs
-        }.getOrElse {
-            DescriptorStatistics.addFailure(this)
-            null
+
+
+    private fun randomObject(): Any {
+        var res: Any? = null
+        while (res == null) {
+            val kfgClass = cm.randomClass
+            if (!kfgClass.isInstantiable || visibilityLevel > kfgClass.visibility) continue
+
+            val klass = ctx.loader.loadClass(kfgClass)
+            res = random.nextOrNull(klass)
         }
+
+        return res
+    }
 
     fun run(attempts: Int = 1000) {
         var successes = 0
@@ -42,17 +50,15 @@ class RandomDescriptorGenerator(val ctx: ExecutionContext, val target: Package, 
         var successDepths = 0
         repeat(attempts) {
             log.debug("Attempt: $it")
-            val kfgClass = cm.randomClass.type
-            val klass = ctx.loader.loadClass(kfgClass)
 
-            val any = random.nextOrNull(klass)
+            val any = randomObject()
             val descriptor = any.descriptor
             val descriptorDepth = descriptor.depth
             depth += descriptorDepth
 
             log.debug("Depth: $descriptorDepth")
             val originalDescriptor = descriptor.deepCopy()
-            val callStack = descriptor.callStack
+            val callStack = `try` { CallStackGenerator(ctx, psa).generate(descriptor) }.getOrNull()
             val generatedAny = callStack?.let { stack ->
                 `try` {
                     CallStackExecutor(ctx).execute(stack)
