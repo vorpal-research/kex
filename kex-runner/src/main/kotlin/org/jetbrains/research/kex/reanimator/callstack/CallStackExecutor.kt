996bcedc7905cdb346300075f6cb0775ec993933
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-09-18 12:54:31 +0300

    fix for CallStackExecutor.kt: correctly generating primary values

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/callstack/CallStackExecutor.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/callstack/CallStackExecutor.kt
@@ -52,79 +52,82 @@ class CallStackExecutor(val ctx: ExecutionContext) {
         TraceCollectorProxy.initializeEmptyCollector(ctx.cm)
 
         var current: Any? = null
-        for (call in callStack) {
-            current = when (call) {
-                is PrimaryValue<*> -> call.value
-                is DefaultConstructorCall -> {
-                    val reflection = ctx.loader.loadClass(call.klass)
-                    val defaultConstructor = reflection.getDeclaredConstructor()
-                    val instance = defaultConstructor.newInstance()
-                    cache[callStack] = instance
-                    instance
-                }
-                is ConstructorCall -> {
-                    val reflection = ctx.loader.loadClass(call.klass)
-                    val constructor = reflection.getConstructor(call.constructor, ctx.loader)
-                    constructor.isAccessible = true
-                    val args = call.args.map { execute(it) }.toTypedArray()
-                    val instance = constructor.newInstance(*args)
-                    cache[callStack] = instance
-                    instance
-                }
-                is ExternalConstructorCall -> {
-                    val reflection = ctx.loader.loadClass(call.constructor.`class`)
-                    val javaMethod = reflection.getMethod(call.constructor, ctx.loader)
-                    javaMethod.isAccessible = true
-                    val args = call.args.map { execute(it) }.toTypedArray()
-                    val instance = javaMethod.invoke(null, *args)
-                    cache[callStack] = instance
-                    instance
-                }
-                is MethodCall -> {
-                    val reflection = ctx.loader.loadClass(call.method.`class`)
-                    val javaMethod = reflection.getMethod(call.method, ctx.loader)
-                    javaMethod.isAccessible = true
-                    val args = call.args.map { execute(it) }.toTypedArray()
-                    javaMethod.invoke(current, *args)
-                    current
-                }
-                is NewArray -> {
-                    val length = execute(call.length)!! as Int
-                    val elementReflection = ctx.loader.loadClass(call.asArray.component)
-                    Array.newInstance(elementReflection, length)
-                }
-                is ArrayWrite -> {
-                    val index = execute(call.index)!! as Int
-                    val value = execute(call.value)
-                    setValue(current!!, index, value)
-                    current
-                }
-                is FieldSetter -> {
-                    val field = call.field
-                    val reflection = ctx.loader.loadClass(field.`class`)
-                    val fieldReflection = reflection.getFieldByName(field.name)
-                    fieldReflection.isAccessible = true
-                    fieldReflection.isFinal = false
-                    val value = execute(call.value)
-                    fieldReflection.setValue(current, value)
-                    current
-                }
-                is StaticFieldSetter -> {
-                    val field = call.field
-                    val reflection = ctx.loader.loadClass(field.`class`)
-                    val fieldReflection = reflection.getFieldByName(field.name)
-                    fieldReflection.isAccessible = true
-                    fieldReflection.isFinal = false
-                    val value = execute(call.value)
-                    fieldReflection.setValue(null, value)
-                    current
-                }
-                is UnknownCall -> {
+        when (callStack) {
+            is PrimaryValue<*> -> current = callStack.value
+            else -> for (call in callStack) {
+                current = when (call) {
+                    is PrimaryValue<*> -> call.value
+                    is DefaultConstructorCall -> {
+                        val reflection = ctx.loader.loadClass(call.klass)
+                        val defaultConstructor = reflection.getDeclaredConstructor()
+                        val instance = defaultConstructor.newInstance()
+                        cache[callStack] = instance
+                        instance
+                    }
+                    is ConstructorCall -> {
+                        val reflection = ctx.loader.loadClass(call.klass)
+                        val constructor = reflection.getConstructor(call.constructor, ctx.loader)
+                        constructor.isAccessible = true
+                        val args = call.args.map { execute(it) }.toTypedArray()
+                        val instance = constructor.newInstance(*args)
+                        cache[callStack] = instance
+                        instance
+                    }
+                    is ExternalConstructorCall -> {
+                        val reflection = ctx.loader.loadClass(call.constructor.`class`)
+                        val javaMethod = reflection.getMethod(call.constructor, ctx.loader)
+                        javaMethod.isAccessible = true
+                        val args = call.args.map { execute(it) }.toTypedArray()
+                        val instance = javaMethod.invoke(null, *args)
+                        cache[callStack] = instance
+                        instance
+                    }
+                    is MethodCall -> {
+                        val reflection = ctx.loader.loadClass(call.method.`class`)
+                        val javaMethod = reflection.getMethod(call.method, ctx.loader)
+                        javaMethod.isAccessible = true
+                        val args = call.args.map { execute(it) }.toTypedArray()
+                        javaMethod.invoke(current, *args)
+                        current
+                    }
+                    is NewArray -> {
+                        val length = execute(call.length)!! as Int
+                        val elementReflection = ctx.loader.loadClass(call.asArray.component)
+                        Array.newInstance(elementReflection, length)
+                    }
+                    is ArrayWrite -> {
+                        val index = execute(call.index)!! as Int
+                        val value = execute(call.value)
+                        setValue(current!!, index, value)
+                        current
+                    }
+                    is FieldSetter -> {
+                        val field = call.field
+                        val reflection = ctx.loader.loadClass(field.`class`)
+                        val fieldReflection = reflection.getFieldByName(field.name)
+                        fieldReflection.isAccessible = true
+                        fieldReflection.isFinal = false
+                        val value = execute(call.value)
+                        fieldReflection.setValue(current, value)
+                        current
+                    }
+                    is StaticFieldSetter -> {
+                        val field = call.field
+                        val reflection = ctx.loader.loadClass(field.`class`)
+                        val fieldReflection = reflection.getFieldByName(field.name)
+                        fieldReflection.isAccessible = true
+                        fieldReflection.isFinal = false
+                        val value = execute(call.value)
+                        fieldReflection.setValue(null, value)
+                        current
+                    }
+                    is UnknownCall -> {
 //                    val reflection = ctx.loader.loadClass(call.type)
 //                    ctx.random.nextOrNull(reflection)
-                    null
+                        null
+                    }
+                    else -> null
                 }
-                else -> null
             }
         }
 
