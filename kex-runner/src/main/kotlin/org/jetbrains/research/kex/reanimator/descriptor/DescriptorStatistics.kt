bfe7efca1006ddcea8af827ba3d5b15ca8e7788d
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-11-23 13:09:57 +0300

    counting non-trivial descriptor depth in kex mode

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/descriptor/DescriptorStatistics.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/descriptor/DescriptorStatistics.kt
@@ -11,6 +11,8 @@ object DescriptorStatistics {
     private var successes = 0
     private var successTime = 0L
     private var failTime = 0L
+    private var nonTrivialDepth = 0L
+    private var nonTrivialCount = 0L
 
     val CallStack.isComplete: Boolean get() {
         val visited = mutableSetOf<CallStack>()
@@ -34,7 +36,12 @@ object DescriptorStatistics {
     }
 
     fun addDescriptor(descriptor: Descriptor, callStack: CallStack, time: Long) {
-        depth += descriptor.depth
+        val descDepth = descriptor.depth
+        depth += descDepth
+        if (descDepth > 1) {
+            nonTrivialCount++
+            nonTrivialDepth += descDepth
+        }
         when {
             callStack.isComplete -> {
                 ++successes
@@ -57,6 +64,7 @@ object DescriptorStatistics {
 //        log.info("Unknown descriptors: ${failures.joinToString("\nDescriptor:\n", prefix = "\n")}")
         log.info("Descriptor generation: ${String.format("%.2f", successRate * 100)}%")
         log.info("Average descriptor depth: ${String.format("%.2f", depth.toDouble() / totalSize)}")
+        log.info("Average non-trivial descriptor depth: ${String.format("%.2f", nonTrivialDepth.toDouble() / nonTrivialCount)}")
         log.info("Average time per successful descriptor generation: ${String.format("%.02f", successTime.toDouble() / successes)}")
         log.info("Average time per failed descriptor generation: ${String.format("%.02f", failTime.toDouble() / failures.size)}")
         log.info("Average time per descriptor generation: ${String.format("%.02f", (successTime + failTime).toDouble() / totalSize)}")
