7e568669982fc395875958ccb8322bb6e778655d
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-01-20 13:32:22 +0300

    fix: filtering external constructors with invalid visibility

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/collector/ExternalConstructorCollector.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/reanimator/collector/ExternalConstructorCollector.kt
@@ -1,5 +1,8 @@
 package org.jetbrains.research.kex.reanimator.collector
 
+import org.jetbrains.research.kex.asm.util.Visibility
+import org.jetbrains.research.kex.asm.util.visibility
+import org.jetbrains.research.kex.config.kexConfig
 import org.jetbrains.research.kfg.ClassManager
 import org.jetbrains.research.kfg.ir.Class
 import org.jetbrains.research.kfg.ir.Method
@@ -7,6 +10,7 @@ import org.jetbrains.research.kfg.type.ClassType
 import org.jetbrains.research.kfg.visitor.MethodVisitor
 
 val Class.externalConstructors: Set<Method> get() = ExternalConstructorCollector.externalConstructors.getOrDefault(this, setOf())
+private val visibilityLevel by lazy { kexConfig.getEnumValue("apiGeneration", "visibility", true, Visibility.PUBLIC) }
 
 class ExternalConstructorCollector(override val cm: ClassManager) : MethodVisitor {
     companion object {
@@ -18,6 +22,7 @@ class ExternalConstructorCollector(override val cm: ClassManager) : MethodVisito
     override fun visit(method: Method) {
         val returnType = (method.returnType as? ClassType) ?: return
         if (!(method.isStatic && method.argTypes.all { !it.isSubtypeOf(returnType) } && !method.isSynthetic)) return
+        if (visibilityLevel > method.visibility) return
 
         externalConstructors.getOrPut(returnType.`class`, ::mutableSetOf) += method
         returnType.`class`.allAncestors.forEach {
