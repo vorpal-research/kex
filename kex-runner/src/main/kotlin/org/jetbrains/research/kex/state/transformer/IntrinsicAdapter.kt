093f0379825f67c347a932d61d763e0c0fc417c0
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-05-11 13:18:14 +0300

    temporarily rollback intrinsics adapter changes

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/state/transformer/IntrinsicAdapter.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/state/transformer/IntrinsicAdapter.kt
@@ -14,8 +14,8 @@ import org.jetbrains.research.kfg.ir.value.instruction.ArrayStoreInst
 import org.jetbrains.research.kthelper.collection.dequeOf
 import java.util.*
 
-object IntrinsicAdapter : RecollectingTransformer<IntrinsicAdapter> {
-    override val builders: Deque<StateBuilder> = dequeOf(StateBuilder())
+object IntrinsicAdapter : Transformer<IntrinsicAdapter> {
+//    override val builders: Deque<StateBuilder> = dequeOf(StateBuilder())
     private val im = MethodManager.IntrinsicManager
     private val kim = MethodManager.KexIntrinsicManager
 
@@ -33,12 +33,12 @@ object IntrinsicAdapter : RecollectingTransformer<IntrinsicAdapter> {
             im.checkParameterIsNotNull(method.cm) -> assume { call.arguments[0] inequality null }
             im.checkNotNullParameter(method.cm) -> assume { call.arguments[0] inequality null }
             im.areEqual(method.cm) -> state { predicate.lhv equality (call.arguments[0] eq call.arguments[1]) }
-            kim.kexAssume(method.cm) -> {
-                for (assert in getAllAssertions(method, call.arguments[0])) {
-                    currentBuilder += assume { assert equality true }
-                }
-                nothing()
-            }
+//            kim.kexAssume(method.cm) -> {
+//                for (assert in getAllAssertions(method, call.arguments[0])) {
+//                    currentBuilder += assume { assert equality true }
+//                }
+//                nothing()
+//            }
             else -> predicate
         }
     }
