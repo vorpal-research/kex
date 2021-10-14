abf354883ca21ba5304aaf18042d05a9e15c8739
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2021-10-14 17:28:55 +0300

    implemented #31

--- kex-runner/src/main/kotlin/org/jetbrains/research/kex/launcher/ConcolicLauncher.kt
+++ kex-runner/src/main/kotlin/org/jetbrains/research/kex/launcher/ConcolicLauncher.kt
@@ -7,10 +7,10 @@ import org.jetbrains.research.kex.asm.analysis.concolic.InstructionConcolicCheck
 import org.jetbrains.research.kex.asm.manager.MethodWrapperInitializer
 import org.jetbrains.research.kex.asm.transform.SymbolicTraceCollector
 import org.jetbrains.research.kex.asm.transform.SystemExitTransformer
+import org.jetbrains.research.kex.jacoco.CoverageReporter
 import org.jetbrains.research.kex.trace.symbolic.InstructionTraceManager
 import org.jetbrains.research.kfg.visitor.MethodVisitor
 import org.jetbrains.research.kthelper.logging.log
-import java.util.*
 
 @ExperimentalSerializationApi
 @InternalSerializationApi
@@ -21,19 +21,12 @@ class ConcolicLauncher(classPaths: List<String>, targetName: String) : KexLaunch
 
     override fun launch() {
         val traceManager = InstructionTraceManager()
-        val cm = createCoverageCounter(context.cm, traceManager)
 
         runPipeline(context) {
             +MethodWrapperInitializer(context.cm)
             +SystemExitTransformer(context.cm)
             +InstructionConcolicChecker(context, traceManager)
-            +cm
         }
-        val coverage = cm.totalCoverage
-        log.info(
-            "Overall summary for ${cm.methodInfos.size} methods:\n" +
-                    "body coverage: ${String.format(Locale.ENGLISH, "%.2f", coverage.bodyCoverage)}%\n" +
-                    "full coverage: ${String.format(Locale.ENGLISH, "%.2f", coverage.fullCoverage)}%"
-        )
+        log.info(CoverageReporter(pkg, containerClassLoader).execute())
     }
 }
