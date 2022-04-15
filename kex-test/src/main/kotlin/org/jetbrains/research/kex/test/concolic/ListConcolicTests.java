f7dcde65e1943b2f895aabf7551cc75f69eae048
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-04-15 19:04:33 +0300

    more fixes

--- kex-test/src/main/kotlin/org/jetbrains/research/kex/test/concolic/ListConcolicTests.java
+++ kex-test/src/main/kotlin/org/jetbrains/research/kex/test/concolic/ListConcolicTests.java
@@ -3,6 +3,7 @@ package org.jetbrains.research.kex.test.concolic;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+@SuppressWarnings("ALL")
 public class ListConcolicTests {
     public static void testArrayList(ArrayList<Point> points) {
         if (points.get(0).getX() == 10) {
@@ -13,6 +14,14 @@ public class ListConcolicTests {
     public static void testLinkedList(LinkedList<Point> points) {
         if (points.get(0).getX() == 10) {
             throw new IllegalStateException();
+        }
+    }
+
+    public static void testArrayListIterator(ArrayList<Character> chars) {
+        for (Character character : chars) {
+            if (character.charValue() == 'v') {
+                throw new IllegalStateException();
+            }
         }
     }
 }
