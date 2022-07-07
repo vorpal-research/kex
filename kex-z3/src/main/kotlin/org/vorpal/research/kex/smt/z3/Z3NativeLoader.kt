4a72d2bebb20b2142613cee1caf4e496d166e579
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-07-07 15:23:11 +0300

    downloading z3 natives from github during build

--- kex-z3/src/main/kotlin/org/vorpal/research/kex/smt/z3/Z3NativeLoader.kt
+++ kex-z3/src/main/kotlin/org/vorpal/research/kex/smt/z3/Z3NativeLoader.kt
@@ -7,11 +7,13 @@ import org.vorpal.research.kthelper.assert.ktassert
 import org.vorpal.research.kthelper.assert.unreachable
 import org.vorpal.research.kthelper.logging.log
 import org.vorpal.research.kthelper.tryOrNull
+import kotlin.io.path.listDirectoryEntries
 
 abstract class Z3NativeLoader {
     companion object {
         const val Z3_VERSION = "4.8.17"
         private val libraries = listOf("libz3", "libz3java")
+        private val vcWinLibrariesToLoadBefore = listOf("vcruntime140", "vcruntime140_1")
         private val supportedArchs = setOf("amd64", "x86_64")
         private val initializeCallback by lazy {
             System.setProperty("z3.skipLibraryLoad", "true")
@@ -24,6 +26,12 @@ abstract class Z3NativeLoader {
                 osProperty.startsWith("linux") -> {
                     "z3-$Z3_VERSION-linux64-native.zip" to libraries.map { "$it.so" }
                 }
+                osProperty.startsWith("windows") -> {
+                    "z3-$Z3_VERSION-win64-native.zip" to (vcWinLibrariesToLoadBefore + libraries).map { "$it.dll" }
+                }
+                osProperty.startsWith("mac") -> {
+                    "z3-$Z3_VERSION-osx64-native.zip" to libraries.map { "$it.dylib" }
+                }
                 else -> unreachable { log.error("Unknown OS: $osProperty") }
             }
 
@@ -31,12 +39,20 @@ abstract class Z3NativeLoader {
                 Z3NativeLoader::class.java.classLoader.getResourceAsStream(zipName)
                     ?: unreachable { log.error("z3 archive not found") },
                 "lib"
-            )
+            ).listDirectoryEntries().first()
 
-            val resolvedLibraries = libraryNames.map { tempDir.resolve(it).toAbsolutePath().toString() }
-            resolvedLibraries.forEach { System.load(it) }
-
-            tryOrNull { deleteDirectory(tempDir) }
+            try {
+                val resolvedLibraries = libraryNames.map { lib ->
+                    tempDir
+                        .resolve("bin")
+                        .resolve(lib)
+                        .toAbsolutePath()
+                        .toString()
+                }
+                resolvedLibraries.forEach { System.load(it) }
+            } finally {
+                tryOrNull { deleteDirectory(tempDir) }
+            }
         }
     }
 
