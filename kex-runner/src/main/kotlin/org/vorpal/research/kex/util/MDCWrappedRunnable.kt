7679393b929abe548df14acc90e7dbe803253e87
Azat Abdullin <azat.aam@gmail.com>
2023-07-18 13:15:23 +0300

    fix wrong MDC context capture in coroutines

--- kex-runner/src/main/kotlin/org/vorpal/research/kex/util/MDCWrappedRunnable.kt
+++ kex-runner/src/main/kotlin/org/vorpal/research/kex/util/MDCWrappedRunnable.kt
@@ -6,8 +6,10 @@ import org.slf4j.MDC
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
-class MDCWrappedRunnable(private val runnable: Runnable) : Runnable {
-    private val context: Map<String, String>? = MDC.getCopyOfContextMap()
+class MDCWrappedRunnable(
+    private val context: Map<String, String>? = MDC.getCopyOfContextMap(),
+    private val runnable: Runnable
+) : Runnable {
     override fun run() {
         try {
             context?.let { context -> MDC.setContextMap(context) }
@@ -21,8 +23,12 @@ class MDCWrappedRunnable(private val runnable: Runnable) : Runnable {
 fun newFixedThreadPoolContextWithMDC(nThreads: Int, name: String): ExecutorCoroutineDispatcher {
     require(nThreads >= 1) { "Expected at least one thread, but $nThreads specified" }
     val threadNo = AtomicInteger()
+    val context = MDC.getCopyOfContextMap()
     val executor = Executors.newScheduledThreadPool(nThreads) { runnable ->
-        val t = Thread(MDCWrappedRunnable(runnable), if (nThreads == 1) name else name + "-" + threadNo.incrementAndGet())
+        val t = Thread(
+            MDCWrappedRunnable(context, runnable),
+            if (nThreads == 1) name else name + "-" + threadNo.incrementAndGet()
+        )
         t.isDaemon = true
         t
     }
