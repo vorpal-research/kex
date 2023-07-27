14fac72ba2032133f9bdb2c9be1230ae544301ca
Azat Abdullin <azat.aam@gmail.com>
2023-07-27 16:55:45 +0300

    Kill executor before running code coverage

--- kex-runner/src/main/kotlin/org/vorpal/research/kex/launcher/ConcolicLauncher.kt
+++ kex-runner/src/main/kotlin/org/vorpal/research/kex/launcher/ConcolicLauncher.kt
@@ -41,16 +41,16 @@ class ConcolicLauncher(classPaths: List<String>, targetName: String) : KexAnalys
             for (setOfTargets in batchedTargets) {
                 InstructionConcolicChecker.run(context, setOfTargets)
             }
+        }
 
-            if (kexConfig.getBooleanValue("kex", "computeCoverage", true)) {
-                val coverageInfo = CoverageReporter(containers).execute(context.cm, analysisLevel)
-                log.info(
-                    coverageInfo.print(kexConfig.getBooleanValue("kex", "printDetailedCoverage", false))
-                )
+        if (kexConfig.getBooleanValue("kex", "computeCoverage", true)) {
+            val coverageInfo = CoverageReporter(containers).execute(context.cm, analysisLevel)
+            log.info(
+                coverageInfo.print(kexConfig.getBooleanValue("kex", "printDetailedCoverage", false))
+            )
 
-                PermanentCoverageInfo.putNewInfo("concolic", analysisLevel.toString(), coverageInfo)
-                PermanentCoverageInfo.emit()
-            }
+            PermanentCoverageInfo.putNewInfo("concolic", analysisLevel.toString(), coverageInfo)
+            PermanentCoverageInfo.emit()
         }
     }
 }
