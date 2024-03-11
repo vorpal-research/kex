76f9e0dee60a9816d87633144841917e6a554798
ravsemirnov <vsemirnov.roman@gmail.com>
2024-03-11 10:07:15 +0100

    new Path Selectors and add of path selector choice in kek.ini.

--- kex-runner/src/main/kotlin/org/vorpal/research/kex/asm/analysis/symbolic/InstructionSymbolicChecker.kt
+++ kex-runner/src/main/kotlin/org/vorpal/research/kex/asm/analysis/symbolic/InstructionSymbolicChecker.kt
@@ -17,10 +17,21 @@ class InstructionSymbolicChecker(
     ctx: ExecutionContext,
     rootMethod: Method,
 ) : SymbolicTraverser(ctx, rootMethod) {
-    override val pathSelector: SymbolicPathSelector = NSubpathPathSelector(2)
-//    override val pathSelector: SymbolicPathSelector = DequePathSelector()
+    override val pathSelector: SymbolicPathSelector
     override val callResolver: SymbolicCallResolver = DefaultCallResolver(ctx)
     override val invokeDynamicResolver: SymbolicInvokeDynamicResolver = DefaultCallResolver(ctx)
+
+    init {
+        val pathSelectorName: String = kexConfig.getStringValue("kex", "pathSelector", "NSubpathPathSelector")
+        val n: Int = kexConfig.getIntValue("kex", "nPathSelectorArg", 2)
+        pathSelector = when (pathSelectorName) {
+            "DequePathSelector" -> DequePathSelector()
+            "NSubpathPathSelector" -> NSubpathPathSelector(n)
+            "PriorityDequePathSelector" -> PriorityDequePathSelector(n)
+            else -> throw IllegalArgumentException("PathSelector '$pathSelectorName' doesn't exist. " +
+                    "Check InstructionSymbolicChecker to see available path selectors")
+        }
+    }
 
     companion object {
         @ExperimentalTime
--- kex-runner/src/main/kotlin/org/vorpal/research/kex/asm/analysis/symbolic/SymbolicPathSelector.kt
+++ kex-runner/src/main/kotlin/org/vorpal/research/kex/asm/analysis/symbolic/SymbolicPathSelector.kt
@@ -1,8 +1,11 @@
 package org.vorpal.research.kex.asm.analysis.symbolic
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.vorpal.research.kex.asm.analysis.util.SuspendableIterator
 import org.vorpal.research.kfg.ir.BasicBlock
 import org.vorpal.research.kthelper.collection.queueOf
+import java.io.File
 import kotlin.math.min
 
 interface SymbolicPathSelector : SuspendableIterator<Pair<TraverserState, BasicBlock>> {
@@ -11,11 +14,25 @@ interface SymbolicPathSelector : SuspendableIterator<Pair<TraverserState, BasicB
     suspend operator fun plusAssign(data: Pair<TraverserState, BasicBlock>) = add(data.first, data.second)
 }
 
+/**
+ * Implements bfs algorithm
+ */
 class DequePathSelector : SymbolicPathSelector {
     private val queue = queueOf<Pair<TraverserState, BasicBlock>>()
 
+    init {
+        val file = File("/Users/ravsemirnov/Desktop/JetBrains/projects/kex/checkPathSelector.txt")
+        val writer = file.bufferedWriter()
+        writer.write("I USE DequePathSelector")
+    }
+
     override suspend fun add(state: TraverserState, block: BasicBlock) {
         queue += state to block
+        val file = File("/Users/ravsemirnov/Desktop/JetBrains/projects/kex/checkPathSelector.txt")
+        val writer = file.bufferedWriter()
+        withContext(Dispatchers.IO) {
+            writer.write("I USE DequePathSelector")
+        }
     }
 
     override suspend fun hasNext(): Boolean = queue.isNotEmpty()
@@ -23,9 +40,66 @@ class DequePathSelector : SymbolicPathSelector {
     override suspend fun next(): Pair<TraverserState, BasicBlock> = queue.poll()
 }
 
+/**
+ * Implements n-subpath algorithm, which makes path decisions based on the frequency of visits to paths of length n
+ */
 class NSubpathPathSelector(val n: Int = 2) : SymbolicPathSelector {
     private val eSVector = mutableSetOf<ExecutionState>()
+    private val pathVisits = mutableMapOf<List<BasicBlock>, Int>()
+
+    init {
+        val file = File("/Users/ravsemirnov/Desktop/JetBrains/projects/kex/checkPathSelector.txt")
+        val writer = file.bufferedWriter()
+        writer.write("I USE NSubpathPathSelector")
+    }
+
+    override suspend fun add(state: TraverserState, block: BasicBlock) {
+        val newState = ExecutionState(state to block, n)
+        eSVector.add(newState)
+        if (pathVisits[newState.path] == null) {
+            pathVisits[newState.path] = 0
+        }
+    }
+
+    override suspend fun hasNext(): Boolean = eSVector.isNotEmpty()
+
+    override suspend fun next(): Pair<TraverserState, BasicBlock> {
+        var minPath = Int.MAX_VALUE
+        eSVector.forEach { minPath = min(minPath, pathVisits[it.path]!!) }
+        val selectSet = mutableSetOf<ExecutionState>()
+
+        for (elem in eSVector) {
+            if (pathVisits[elem.path] == minPath) {
+                selectSet.add(elem)
+            }
+        }
+
+        val result =  selectSet.random()
+        pathVisits[result.path] = pathVisits[result.path]!! + 1
+        eSVector.remove(result)
+        return result.node
+    }
+
+    class ExecutionState(
+        val node: Pair<TraverserState, BasicBlock>,
+        pathLength: Int
+    ) {
+        val path: List<BasicBlock> = node.first.blockPath.takeLast(pathLength)
+    }
+}
+
+/**
+ * Implements bfs algorithm, which takes into account frequency of visits to paths of length n from n-subpath
+ */
+class PriorityDequePathSelector(val n: Int = 2) : SymbolicPathSelector {
+    private val eSVector = mutableSetOf<ExecutionState>()
     private val p = mutableMapOf<List<BasicBlock>, Int>()
+
+    init {
+        val file = File("/Users/ravsemirnov/Desktop/JetBrains/projects/kex/checkPathSelector.txt")
+        val writer = file.bufferedWriter()
+        writer.write("I USE PriorityDequePathSelector")
+    }
 
     override suspend fun add(state: TraverserState, block: BasicBlock) {
         val newState = ExecutionState(state to block, n)
@@ -38,17 +112,15 @@ class NSubpathPathSelector(val n: Int = 2) : SymbolicPathSelector {
     override suspend fun hasNext(): Boolean = eSVector.isNotEmpty()
 
     override suspend fun next(): Pair<TraverserState, BasicBlock> {
+        val minLen = eSVector.minOf { it.path.size }
+        val filteredESVector = eSVector.filter { it.path.size == minLen }
+
         var minPath = Int.MAX_VALUE
-        eSVector.forEach { minPath = min(minPath, p[it.path]!!) }
-        val selectSet = mutableSetOf<ExecutionState>()
+        filteredESVector.forEach { minPath = min(minPath, p[it.path]!!) }
+        val filteredESVector2 = filteredESVector.filter { p[it.path] == minPath }
 
-        for (elem in eSVector) {
-            if (p[elem.path] == minPath) {
-                selectSet.add(elem)
-            }
-        }
+        val result =  filteredESVector2.random()
 
-        val result =  selectSet.random()
         p[result.path] = p[result.path]!! + 1
         eSVector.remove(result)
         return result.node
