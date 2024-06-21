c1d864e420ffd1e3cfcadcbe8e61f14cd020ad76
FerrumBrain <lemur1416@gmail.com>
2024-06-21 15:44:40 +0200

    added timeout handling for kex

--- kex-core/src/main/kotlin/org/vorpal/research/kex/trace/symbolic/SymbolicTraceBuilder.kt
+++ kex-core/src/main/kotlin/org/vorpal/research/kex/trace/symbolic/SymbolicTraceBuilder.kt
@@ -6,11 +6,7 @@ import org.vorpal.research.kex.ExecutionContext
 import org.vorpal.research.kex.asm.manager.MethodManager
 import org.vorpal.research.kex.asm.state.asTermExpr
 import org.vorpal.research.kex.asm.transform.SymbolicTraceInstrumenter
-import org.vorpal.research.kex.descriptor.ConstantDescriptor
-import org.vorpal.research.kex.descriptor.Descriptor
-import org.vorpal.research.kex.descriptor.Object2DescriptorConverter
-import org.vorpal.research.kex.descriptor.ObjectDescriptor
-import org.vorpal.research.kex.descriptor.descriptor
+import org.vorpal.research.kex.descriptor.*
 import org.vorpal.research.kex.ktype.*
 import org.vorpal.research.kex.parameters.Parameters
 import org.vorpal.research.kex.state.predicate.Predicate
@@ -19,42 +15,9 @@ import org.vorpal.research.kex.state.predicate.state
 import org.vorpal.research.kex.state.term.*
 import org.vorpal.research.kex.state.transformer.TermRenamer
 import org.vorpal.research.kex.util.*
-import org.vorpal.research.kfg.ir.BasicBlock
-import org.vorpal.research.kfg.ir.Class
-import org.vorpal.research.kfg.ir.ConcreteClass
-import org.vorpal.research.kfg.ir.Method
-import org.vorpal.research.kfg.ir.MethodDescriptor
-import org.vorpal.research.kfg.ir.value.Argument
-import org.vorpal.research.kfg.ir.value.Constant
-import org.vorpal.research.kfg.ir.value.NameMapperContext
-import org.vorpal.research.kfg.ir.value.NullConstant
-import org.vorpal.research.kfg.ir.value.ThisRef
-import org.vorpal.research.kfg.ir.value.Value
-import org.vorpal.research.kfg.ir.value.instruction.ArrayLoadInst
-import org.vorpal.research.kfg.ir.value.instruction.ArrayStoreInst
-import org.vorpal.research.kfg.ir.value.instruction.BinaryInst
-import org.vorpal.research.kfg.ir.value.instruction.BranchInst
-import org.vorpal.research.kfg.ir.value.instruction.CallInst
-import org.vorpal.research.kfg.ir.value.instruction.CastInst
-import org.vorpal.research.kfg.ir.value.instruction.CatchInst
-import org.vorpal.research.kfg.ir.value.instruction.CmpInst
-import org.vorpal.research.kfg.ir.value.instruction.EnterMonitorInst
-import org.vorpal.research.kfg.ir.value.instruction.ExitMonitorInst
-import org.vorpal.research.kfg.ir.value.instruction.FieldLoadInst
-import org.vorpal.research.kfg.ir.value.instruction.FieldStoreInst
-import org.vorpal.research.kfg.ir.value.instruction.Handle
-import org.vorpal.research.kfg.ir.value.instruction.InstanceOfInst
-import org.vorpal.research.kfg.ir.value.instruction.Instruction
-import org.vorpal.research.kfg.ir.value.instruction.InvokeDynamicInst
-import org.vorpal.research.kfg.ir.value.instruction.JumpInst
-import org.vorpal.research.kfg.ir.value.instruction.NewArrayInst
-import org.vorpal.research.kfg.ir.value.instruction.NewInst
-import org.vorpal.research.kfg.ir.value.instruction.PhiInst
-import org.vorpal.research.kfg.ir.value.instruction.ReturnInst
-import org.vorpal.research.kfg.ir.value.instruction.SwitchInst
-import org.vorpal.research.kfg.ir.value.instruction.TableSwitchInst
-import org.vorpal.research.kfg.ir.value.instruction.ThrowInst
-import org.vorpal.research.kfg.ir.value.instruction.UnaryInst
+import org.vorpal.research.kfg.ir.*
+import org.vorpal.research.kfg.ir.value.*
+import org.vorpal.research.kfg.ir.value.instruction.*
 import org.vorpal.research.kfg.type.SystemTypeNames
 import org.vorpal.research.kfg.type.Type
 import org.vorpal.research.kfg.type.parseDescOrNull
@@ -80,6 +43,11 @@ class SymbolicTraceBuilder(
 ) : SymbolicState(), InstructionTraceCollector {
     companion object {
         private const val MAX_ARRAY_LENGTH = 10000
+        private var killSwitch: Boolean = false
+
+        fun setKillSwitch(value: Boolean) {
+            killSwitch = value
+        }
     }
 
     /**
@@ -476,6 +444,7 @@ class SymbolicTraceBuilder(
      * for more information @see(SymbolicTraceBuilder.call), @see(SymbolicTraceBuilder.jump), @see(SymbolicTraceBuilder.ret)
      */
     private fun postProcess(instruction: Instruction, predicate: Predicate) {
+        checkTimeout()
         postProcess(StateClause(instruction, predicate))
     }
 
@@ -1481,5 +1450,13 @@ class SymbolicTraceBuilder(
 
     override fun resetConverter() {
         converter = Object2DescriptorConverter()
+    }
+
+    private fun checkTimeout() {
+        if (!traceCollectingEnabled) return
+
+        if (killSwitch) {
+            throw SymbolicTraceException("timeout", TimeoutException())
+        }
     }
 }
